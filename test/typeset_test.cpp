2c0ba410903b559c9470f7e8252b1903355c3f01
attu <attu@archie>
2014-09-18 19:05:11 +0200

    type set size

--- test/typeset_test.cpp
+++ test/typeset_test.cpp
@@ -20,13 +20,15 @@ struct TypeSet : TypeSet<Head>, TypeSet<Tail...> {
     return TypeSet<Head>::template get<T>();
   }
   template <typename T, Requires<Not<std::is_same<T, Head>>>...>
-  const T& get() const {
+  T const& get() const {
     return TypeSet<Tail...>::template get<T>();
   }
   template <typename T, Requires<Not<std::is_same<T, Head>>>...>
   T& get() {
     return TypeSet<Tail...>::template get<T>();
   }
+
+  constexpr std::size_t size() const { return 1 + sizeof...(Tail); }
 };
 
 template <typename Head>
@@ -36,7 +38,7 @@ struct TypeSet<Head> {
       : value(std::forward<U>(u)) {}
 
   template <typename T>
-  const T& get() const {
+  T const& get() const {
     static_assert(std::is_same<T, Head>::value, "Type not found in TypeSet");
     return value;
   }
@@ -46,18 +48,30 @@ struct TypeSet<Head> {
     return value;
   }
 
+  constexpr std::size_t size() const { return 1; }
+
 private:
   Head value;
 };
 
 template <typename T, typename... Types>
-const T& get(const TypeSet<Types...>& ts) {
+const T& get(TypeSet<Types...> const& ts) {
   return ts.get<T>();
 }
 
 template <typename T, typename... Types>
 T& get(TypeSet<Types...>& ts) {
   return ts.get<T>();
+}
+
+template <typename... Types>
+TypeSet<Types...> make_type_set(Types&&... args) {
+  return TypeSet<Types...>(std::forward<Types>(args)...);
+}
+
+template <typename... Types>
+constexpr std::size_t type_set_size(TypeSet<Types...> const&) {
+  return sizeof...(Types);
 }
 }
 }
@@ -69,6 +83,7 @@ struct typeset_test : ::testing::Test {};
 TEST_F(typeset_test, nothing) {
   au::TypeSet<unsigned> unary(1);
   EXPECT_EQ(1, unary.get<unsigned>());
+  EXPECT_EQ(1, unary.size());
 
   au::TypeSet<unsigned, float> binary(2, 3.0);
   EXPECT_EQ(2, binary.get<unsigned>());
@@ -79,6 +94,7 @@ TEST_F(typeset_test, nothing) {
 
   EXPECT_EQ(4, binary.get<unsigned>());
   EXPECT_EQ(5.0, binary.get<float>());
+  EXPECT_EQ(2, binary.size());
 
   unsigned u = 7;
   float f = 8.0;
@@ -86,4 +102,10 @@ TEST_F(typeset_test, nothing) {
   au::TypeSet<float, unsigned> other(f, u);
   EXPECT_EQ(7, au::get<unsigned>(other));
   EXPECT_EQ(8.0, au::get<float>(other));
+
+  auto triplet = au::make_type_set(1, 2.0, std::string("name"));
+  EXPECT_EQ(1, au::get<int>(triplet));
+  EXPECT_EQ(2.0, au::get<double>(triplet));
+  EXPECT_EQ("name", au::get<std::string>(triplet));
+  static_assert(au::type_set_size(triplet) == 3, "");
 }
