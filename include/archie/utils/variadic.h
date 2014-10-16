bd3b15cd4fc6d99b362613c3110c58e396904a42
attu <attu@archie>
2014-10-16 20:00:14 +0200

    get

--- include/archie/utils/variadic.h
+++ include/archie/utils/variadic.h
@@ -15,7 +15,11 @@ namespace utils {
   }
 
   template <typename... Tp>
+  struct TypeSet {};
+
+  template <typename... Tp>
   struct Tuple {
+  private:
     template <typename... Args>
     static decltype(auto) make_storage(Args&&... args) {
       return [args...](auto&& func) -> decltype(auto) {
@@ -25,6 +29,9 @@ namespace utils {
     using Storage = decltype(make_storage(std::declval<Tp>()...));
     Storage storage;
 
+  public:
+    using type = TypeSet<Tp...>;
+
     template <typename... Up>
     explicit Tuple(Up&&... u)
         : storage(make_storage(std::forward<Up>(u)...)) {}
@@ -32,7 +39,37 @@ namespace utils {
     Tuple() : Tuple(Tp {}...) {}
 
     std::size_t size() const { return sizeof...(Tp); }
+
+    template <typename Func>
+    decltype(auto) apply(Func&& func) {
+      return storage(std::forward<Func>(func));
+    }
   };
+
+  namespace detail {
+    struct at_impl {
+      struct any {
+        template <typename... Tp>
+        constexpr any(Tp&&...) {}
+      };
+      template <std::size_t n, typename = std::make_index_sequence<n>>
+      struct get;
+
+      template <std::size_t n, std::size_t... ignore>
+      struct get<n, std::index_sequence<ignore...>> {
+        template <typename Nth, typename...>
+        constexpr decltype(auto) operator()(decltype(ignore, any{})...,
+                                            Nth&& nth, ...) const {
+          return std::forward<Nth>(nth);
+        }
+      };
+    };
+  }
+
+  template <std::size_t n, typename Tp>
+  decltype(auto) get(Tp&& t) {
+    return std::forward<Tp>(t).apply(detail::at_impl::get<n>{});
+  }
 }
 }
 
--- test/variadic_test.cpp
+++ test/variadic_test.cpp
@@ -3,53 +3,6 @@
 
 #include <archie/utils/variadic.h>
 
-namespace archie {
-namespace utils {
-
-  /*
-    template <typename... Args>
-    decltype(auto) make_capture(Args... args) {
-      return [args...](auto&& func) -> decltype(auto) {
-        return std::forward<decltype(func)>(func)(args...);
-      };
-    }
-
-    template <typename... Tp>
-    struct Tuple {
-      using Capture = decltype(make_capture(std::declval<Tp>()...));
-      Capture capture;
-
-      template <typename... Up>
-      explicit Tuple(Up&&... u)
-          : capture(make_capture(std::forward<Up>(u)...)) {}
-
-      Tuple() : Tuple(Tp {}...) {}
-    };
-    struct eat {
-      template <typename... Up>
-      constexpr eat(Up&&...) {}
-    };
-
-    template <std::size_t n, typename = std::make_index_sequence<n>>
-    struct get_impl;
-
-    template <std::size_t n, std::size_t... ignore>
-    struct get_impl<n, std::index_sequence<ignore...>> {
-      template <typename Nth, typename... Rest>
-      constexpr decltype(auto) operator()(decltype(ignore, eat{})..., Nth&& nth,
-                                          Rest&&...) const {
-        return std::forward<Nth>(nth);
-      }
-    };
-
-    template <std::size_t n, typename TypeSet>
-    decltype(auto) get(TypeSet&& ts) {
-      return std::forward<TypeSet>(ts).capture(get_impl<n>{});
-    }
-  */
-}
-}
-
 #include <gtest/gtest.h>
 namespace au = archie::utils;
 
@@ -67,6 +20,7 @@ TEST_F(variadic_test, canCreateTuple) {
   auto t1 = au::Tuple<char>('d');
   EXPECT_EQ(2u, t0.size());
   EXPECT_EQ(1u, t1.size());
+  EXPECT_EQ('d', au::get<0>(t1));
 }
 
 TEST_F(variadic_test, for_each) {
