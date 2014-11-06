5412172f238dd2ec35173695623f77b418a08be6
attu <attu@archie>
2014-11-06 20:35:15 +0100

    new impl

--- test/bitfields_test.cpp
+++ test/bitfields_test.cpp
@@ -4,102 +4,87 @@
 #include <utility>
 #include <tuple>
 
-using size_type = std::size_t;
+namespace archie {
+namespace utils {
 
-template <size_type N>
-using Number = std::integral_constant<size_type, N>;
+  using size_type = std::uint8_t;
 
-namespace detail {
+  template <size_type N>
+  using Number = std::integral_constant<size_type, N>;
 
-using ByteSize = std::integral_constant<size_type, 8>;
+  using ByteSize = std::integral_constant<size_type, 8>;
 
-template <size_type Size>
-struct GetStorage {
-  static_assert(Size <= ByteSize::value * sizeof(std::uint64_t),
-                "Max possible size exceeded");
-  using type = typename std::conditional<
-      (Size > ByteSize::value * sizeof(std::uint8_t)),
-      typename std::conditional<
-          (Size > ByteSize::value * sizeof(std::uint16_t)),
-          typename std::conditional<
-              (Size > ByteSize::value * sizeof(std::uint32_t)), std::uint64_t,
-              std::uint32_t>::type,
-          std::uint16_t>::type,
-      std::uint8_t>::type;
-};
+  template <size_type Size>
+  struct GetStorage {
+    static_assert(Size <= ByteSize::value * sizeof(std::uint64_t),
+                  "Max possible size exceeded");
+    using type = typename std::conditional<
+        (Size > ByteSize::value * sizeof(std::uint8_t)),
+        typename std::conditional<
+            (Size > ByteSize::value * sizeof(std::uint16_t)),
+            typename std::conditional<
+                (Size > ByteSize::value * sizeof(std::uint32_t)), std::uint64_t,
+                std::uint32_t>::type,
+            std::uint16_t>::type,
+        std::uint8_t>::type;
+  };
 
-template <size_type Size>
-using storage = typename GetStorage<Size>::type;
+  template <size_type Size>
+  using Storage = typename GetStorage<Size>::type;
 
-template <typename...>
-struct Sum;
+  template <size_type I, size_type O, size_type S>
+  struct PackElement {
+    using Index = Number<I>;
+    using Offset = Number<O>;
+    using Size = Number<S>;
+  };
 
-template <typename Head, typename... Args>
-struct Sum<Head, Args...> {
-  using tail = typename Sum<Args...>::type;
-  using type = Number<Head::value + tail::value>;
-};
+  template <size_type I, size_type O, size_type S>
+  constexpr size_type offset_of(PackElement<I, O, S>) {
+    return O;
+  }
 
-template <typename Head>
-struct Sum<Head> {
-  using type = Number<Head::value>;
-};
+  namespace detail {
+    template <size_type...>
+    struct PackImpl;
 
-template <size_type Index, typename... Args>
-struct At {
-  static_assert(Index < sizeof...(Args), "Index out of bounds");
-  using type = std::remove_reference_t<
-      decltype(std::get<Index>(std::declval<std::tuple<Args...>>()))>;
-};
+    template <size_type I, size_type O, size_type S>
+    struct PackImpl<I, O, S> : PackElement<I, O, S> {};
 
-template <size_type...>
-struct Subset;
+    template <size_type I, size_type O, size_type S, size_type... T>
+    struct PackImpl<I, O, S, T...> : PackElement<I, O, S>,
+                                     PackImpl<I + 1, O + S, T...> {};
+  }
 
-template <size_type Head, size_type... Indexes>
-struct Subset<Head, Indexes...> {
-  template <typename... Args>
-  using tail = typename Subset<Indexes...>::template type<Args...>;
-  template <typename... Args>
-  using type = Number<At<Head, Args...>::type::value + tail<Args...>::value>;
-};
-
-template <size_type Head>
-struct Subset<Head> {
-  template <typename... Args>
-  using type = Number<At<Head, Args...>::type::value>;
-};
+  template <size_type... S>
+  struct Pack : detail::PackImpl<0, 0, S...> {
+    constexpr Pack() = default;
+  };
+}
 }
 
-template <typename... Args>
-struct Sum : public detail::Sum<Args...>::type {};
-
-template <size_type Index, typename... Args>
-struct At : public detail::At<Index, Args...>::type {};
-
-template <size_type... Indexes>
-struct Subset {
-  template <typename... Args>
-  struct type : public detail::Subset<Indexes...>::template type<Args...> {};
-};
-
-template <typename... Sizes>
-struct Bitfields {
-  using length = Sum<Sizes...>;
-  using fields = std::tuple<Sizes...>;
-  detail::storage<length::value> storage;
-};
-
 #include <gtest/gtest.h>
+namespace {
+
+namespace au = archie::utils;
 
 struct bitfields_test : public ::testing::Test {};
 
-TEST_F(bitfields_test, nothing) {
-  static_assert(sizeof(Bitfields<Number<1>>) <= sizeof(std::uint8_t), "");
-  static_assert(sizeof(Bitfields<Number<8>>) <= sizeof(std::uint8_t), "");
-  static_assert(Sum<Number<1>, Number<2>, Number<3>>::value == 6, "");
-  static_assert(At<2, Number<1>, Number<2>, Number<3>>::value == 3, "");
-  static_assert(
-      Subset<0, 2>::type<Number<1>, Number<2>, Number<3>, Number<4>>::value ==
-          4,
-      "");
+TEST_F(bitfields_test, storage) {
+  static_assert(sizeof(au::Storage<0>) <= sizeof(std::uint8_t), "");
+  static_assert(sizeof(au::Storage<8>) <= sizeof(std::uint8_t), "");
+  static_assert(sizeof(au::Storage<9>) <= sizeof(std::uint16_t), "");
+  static_assert(sizeof(au::Storage<16>) <= sizeof(std::uint16_t), "");
+  static_assert(sizeof(au::Storage<17>) <= sizeof(std::uint32_t), "");
+  static_assert(sizeof(au::Storage<32>) <= sizeof(std::uint32_t), "");
+  static_assert(sizeof(au::Storage<33>) <= sizeof(std::uint64_t), "");
+  static_assert(sizeof(au::Storage<64>) <= sizeof(std::uint64_t), "");
+}
+
+TEST_F(bitfields_test, pack_element_offset) {
+  constexpr au::Pack<1, 2, 3> pack;
+  static_assert(au::offset_of<0>(pack) == 0, "");
+  static_assert(au::offset_of<1>(pack) == 1, "");
+  static_assert(au::offset_of<2>(pack) == 3, "");
+}
 }
