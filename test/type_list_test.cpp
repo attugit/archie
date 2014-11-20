6d1a47c56e7c073f815109f174b0b49e8951277d
Attu <attu.git@email.com>
2014-11-20 21:13:05 +0100

    type_list transform

--- test/type_list_test.cpp
+++ test/type_list_test.cpp
@@ -9,6 +9,9 @@ namespace utils {
 
     template <template <typename...> class Func>
     using apply = typename Func<Ts...>::type;
+
+    template <template <typename> class Func>
+    using transform = type_list<typename Func<Ts>::type...>;
   };
 }
 }
@@ -16,6 +19,7 @@ namespace utils {
 #include <gtest/gtest.h>
 #include <tuple>
 #include <type_traits>
+#include <memory>
 
 namespace {
 
@@ -25,6 +29,11 @@ struct utype {};
 template <typename... Ts>
 struct tuple_ {
   using type = std::tuple<Ts...>;
+};
+
+template <typename Tp>
+struct uptr_ {
+  using type = std::unique_ptr<Tp>;
 };
 
 namespace au = archie::utils;
@@ -41,5 +50,14 @@ TEST_F(type_list_test, canGetTypeListSize) {
 TEST_F(type_list_test, canApply) {
   using type = au::type_list<utype<0>, utype<1>>::apply<tuple_>;
   static_assert(std::is_same<std::tuple<utype<0>, utype<1>>, type>::value, "");
+}
+
+TEST_F(type_list_test, canTransform) {
+  using type = au::type_list<utype<0>, utype<1>>::transform<uptr_>;
+  static_assert(
+      std::is_same<
+          au::type_list<std::unique_ptr<utype<0>>, std::unique_ptr<utype<1>>>,
+          type>::value,
+      "");
 }
 }
