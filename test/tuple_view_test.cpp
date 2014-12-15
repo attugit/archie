73d8e26d4233578178e1429f82e475ecf036ae0e
Karol Wozniak <attu.git@email.com>
2014-12-15 18:35:14 +0100

    fused::tuple_view header

--- test/tuple_view_test.cpp
+++ test/tuple_view_test.cpp
@@ -1,37 +1,4 @@
-#include <archie/utils/fused/tuple.h>
-#include <functional>
-
-namespace archie {
-namespace utils {
-  namespace fused {
-
-    template <typename Tp>
-    struct value_view : std::reference_wrapper<Tp> {
-      using base_t = std::reference_wrapper<Tp>;
-      using base_t::base_t;
-      template <typename Up>
-      value_view& operator=(Up&& u) {
-        this->get() = u;
-        return *this;
-      }
-    };
-
-    template <typename... Ts>
-    using tuple_view = tuple<value_view<Ts>...>;
-
-    template <typename... Ts>
-    decltype(auto) make_view(Ts&... args) {
-      return tuple<value_view<Ts>...>(args...);
-    }
-
-    template <std::size_t... idx, typename... Ts>
-    decltype(auto) select(tuple<Ts...>& t) {
-      return make_view(get<idx>(t)...);
-    }
-  }
-}
-}
-
+#include <archie/utils/fused/tuple_view.h>
 #include <gtest/gtest.h>
 #include <algorithm>
 #include <vector>
