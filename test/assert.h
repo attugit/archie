80389f37bba07041b1e1df6c5b2fd35e8ee54da4
Karol Wozniak <attu.git@email.com>
2014-12-19 21:48:03 +0100

    bitfields test

--- test/assert.h
+++ test/assert.h
@@ -1,12 +1,14 @@
 #include <cassert>
 
-#define EXPECT_EQ(x, y) assert((x) == (y));
-#define EXPECT_TRUE(x) assert((x) == true);
-#define EXPECT_FALSE(x) assert((x) == false);
-#define EXPECT_LE(x, y) assert((x) <= (y));
-
-#define ASSERT_EQ(x, y) EXPECT_EQ(x, y)
+#define ASSERT_TRUE(x) assert((x) == true);
+#define ASSERT_FALSE(x) assert((x) == false);
+#define ASSERT_EQ(x, y) assert((x) == (y));
 #define ASSERT_NE(x, y) assert((x) != (y));
+#define ASSERT_LE(x, y) assert((x) <= (y));
+
+#define EXPECT_EQ(x, y) ASSERT_EQ(x, y)
+#define EXPECT_TRUE(x) ASSERT_TRUE(x)
+#define EXPECT_FALSE(x) ASSERT_FALSE(x)
+#define EXPECT_LE(x, y) ASSERT_LE(x, y)
 #define EXPECT_NE(x, y) ASSERT_NE(x, y)
 
-#define TEST_F(x, y) void y()
--- test/bitfields_test.cpp
+++ test/bitfields_test.cpp
@@ -1,73 +1,48 @@
 #include <archie/utils/bitfields.h>
-#include <gtest/gtest.h>
-namespace {
+#include <test/assert.h>
 
 namespace au = archie::utils;
+using pack_t = au::Pack<1, 2, 3>;
+using field_t = decltype(std::declval<pack_t>().make_field<1>());
+const au::IndexType i0 = au::IndexType(0);
+const au::IndexType i1 = au::IndexType(1);
+const au::IndexType i2 = au::IndexType(2);
 
-struct bitfields_test : public ::testing::Test {
-  au::IndexType i0 = au::IndexType(0);
-  au::IndexType i1 = au::IndexType(1);
-  au::IndexType i2 = au::IndexType(2);
-  using pack_t = au::Pack<1, 2, 3>;
-  using field_t = decltype(std::declval<pack_t>().make_field<1>());
+static_assert(sizeof(au::Storage<0>) <= sizeof(std::uint8_t), "");
+static_assert(sizeof(au::Storage<8>) <= sizeof(std::uint8_t), "");
+static_assert(sizeof(au::Storage<9>) <= sizeof(std::uint16_t), "");
+static_assert(sizeof(au::Storage<16>) <= sizeof(std::uint16_t), "");
+static_assert(sizeof(au::Storage<17>) <= sizeof(std::uint32_t), "");
+static_assert(sizeof(au::Storage<32>) <= sizeof(std::uint32_t), "");
+static_assert(sizeof(au::Storage<33>) <= sizeof(std::uint64_t), "");
+static_assert(sizeof(au::Storage<64>) <= sizeof(std::uint64_t), "");
+
+static_assert(sizeof(au::Pack<0>) <= sizeof(std::uint8_t), "");
+static_assert(sizeof(au::Pack<3, 5>) <= sizeof(std::uint8_t), "");
+static_assert(sizeof(au::Pack<4, 5>) <= sizeof(std::uint16_t), "");
+static_assert(sizeof(au::Pack<3, 5, 8>) <= sizeof(std::uint16_t), "");
+static_assert(sizeof(au::Pack<10, 5, 2>) <= sizeof(std::uint32_t), "");
+static_assert(sizeof(au::Pack<15, 15, 2>) <= sizeof(std::uint32_t), "");
+static_assert(sizeof(au::Pack<10, 10, 13>) <= sizeof(std::uint64_t), "");
+static_assert(sizeof(au::Pack<30, 20, 10, 4>) <= sizeof(std::uint64_t), "");
+
+constexpr pack_t pack;
+static_assert(au::offset_of<0>(pack) == 0, "");
+static_assert(au::offset_of<1>(pack) == 1, "");
+static_assert(au::offset_of<2>(pack) == 3, "");
+static_assert(au::size_of<0>(pack) == 1, "");
+static_assert(au::size_of<1>(pack) == 2, "");
+static_assert(au::size_of<2>(pack) == 3, "");
+static_assert(pack.size() == 6, "");
+static_assert(pack.length() == 3, "");
+
+static_assert(pack_t::mask_of<0>() == 0b000001, "");
+static_assert(pack_t::mask_of<1>() == 0b000110, "");
+static_assert(pack_t::mask_of<2>() == 0b111000, "");
+
+void pack_make_single_field() {
   pack_t pack;
-
-  void SetUp() { pack.set(0b010101); }
-};
-
-TEST_F(bitfields_test, storage) {
-  static_assert(sizeof(au::Storage<0>) <= sizeof(std::uint8_t), "");
-  static_assert(sizeof(au::Storage<8>) <= sizeof(std::uint8_t), "");
-  static_assert(sizeof(au::Storage<9>) <= sizeof(std::uint16_t), "");
-  static_assert(sizeof(au::Storage<16>) <= sizeof(std::uint16_t), "");
-  static_assert(sizeof(au::Storage<17>) <= sizeof(std::uint32_t), "");
-  static_assert(sizeof(au::Storage<32>) <= sizeof(std::uint32_t), "");
-  static_assert(sizeof(au::Storage<33>) <= sizeof(std::uint64_t), "");
-  static_assert(sizeof(au::Storage<64>) <= sizeof(std::uint64_t), "");
-}
-
-TEST_F(bitfields_test, pack) {
-  static_assert(sizeof(au::Pack<0>) <= sizeof(std::uint8_t), "");
-  static_assert(sizeof(au::Pack<3, 5>) <= sizeof(std::uint8_t), "");
-  static_assert(sizeof(au::Pack<4, 5>) <= sizeof(std::uint16_t), "");
-  static_assert(sizeof(au::Pack<3, 5, 8>) <= sizeof(std::uint16_t), "");
-  static_assert(sizeof(au::Pack<10, 5, 2>) <= sizeof(std::uint32_t), "");
-  static_assert(sizeof(au::Pack<15, 15, 2>) <= sizeof(std::uint32_t), "");
-  static_assert(sizeof(au::Pack<10, 10, 13>) <= sizeof(std::uint64_t), "");
-  static_assert(sizeof(au::Pack<30, 20, 10, 4>) <= sizeof(std::uint64_t), "");
-}
-
-TEST_F(bitfields_test, pack_element_offset) {
-  constexpr pack_t pack;
-  static_assert(au::offset_of<0>(pack) == 0, "");
-  static_assert(au::offset_of<1>(pack) == 1, "");
-  static_assert(au::offset_of<2>(pack) == 3, "");
-}
-
-TEST_F(bitfields_test, pack_element_size) {
-  constexpr pack_t pack;
-  static_assert(au::size_of<0>(pack) == 1, "");
-  static_assert(au::size_of<1>(pack) == 2, "");
-  static_assert(au::size_of<2>(pack) == 3, "");
-}
-
-TEST_F(bitfields_test, pack_size) {
-  constexpr pack_t pack;
-  static_assert(pack.size() == 6, "");
-}
-
-TEST_F(bitfields_test, pack_length) {
-  constexpr pack_t pack;
-  static_assert(pack.length() == 3, "");
-}
-
-TEST_F(bitfields_test, pack_mask_of) {
-  static_assert(pack_t::mask_of<0>() == 0b000001, "");
-  static_assert(pack_t::mask_of<1>() == 0b000110, "");
-  static_assert(pack_t::mask_of<2>() == 0b111000, "");
-}
-
-TEST_F(bitfields_test, pack_make_single_field) {
+  pack.set(0b010101);
   auto field = pack.make_field<0>();
   EXPECT_EQ(0u, field.index());
   EXPECT_EQ(0u, field.offset());
@@ -75,7 +50,9 @@ TEST_F(bitfields_test, pack_make_single_field) {
   EXPECT_EQ(0b000001, field.mask());
 }
 
-TEST_F(bitfields_test, pack_make_many_field) {
+void pack_make_many_field() {
+  pack_t pack;
+  pack.set(0b010101);
   auto f0 = pack.make_field<0>();
   auto f1 = pack.make_field<1>();
   auto f2 = pack.make_field<2>();
@@ -96,7 +73,9 @@ TEST_F(bitfields_test, pack_make_many_field) {
   EXPECT_EQ(0b111000, f2.mask());
 }
 
-TEST_F(bitfields_test, pack_field_value) {
+void pack_field_value() {
+  pack_t pack;
+  pack.set(0b010101);
   auto f0 = pack.make_field<0>();
   auto f1 = pack.make_field<1>();
   auto f2 = pack.make_field<2>();
@@ -105,7 +84,9 @@ TEST_F(bitfields_test, pack_field_value) {
   EXPECT_EQ(0b10, f2.value());
 }
 
-TEST_F(bitfields_test, pack_field_test) {
+void pack_field_test() {
+  pack_t pack;
+  pack.set(0b010101);
   auto f0 = pack.make_field<0>();
   auto f1 = pack.make_field<1>();
   auto f2 = pack.make_field<2>();
@@ -117,7 +98,9 @@ TEST_F(bitfields_test, pack_field_test) {
   EXPECT_FALSE(f2.test(i2));
 }
 
-TEST_F(bitfields_test, pack_field_set) {
+void pack_field_set() {
+  pack_t pack;
+  pack.set(0b010101);
   auto field = pack.make_field<1>();
   ASSERT_FALSE(field.test(i0));
   ASSERT_TRUE(field.test(i1));
@@ -130,7 +113,9 @@ TEST_F(bitfields_test, pack_field_set) {
   EXPECT_EQ(0b010111, pack.get());
 }
 
-TEST_F(bitfields_test, pack_field_reset) {
+void pack_field_reset() {
+  pack_t pack;
+  pack.set(0b010101);
   auto field = pack.make_field<1>();
   ASSERT_FALSE(field.test(i0));
   ASSERT_TRUE(field.test(i1));
@@ -143,7 +128,8 @@ TEST_F(bitfields_test, pack_field_reset) {
   EXPECT_EQ(0b010001, pack.get());
 }
 
-TEST_F(bitfields_test, pack_field_all) {
+void pack_field_all() {
+  pack_t pack;
   pack.set(0b010110);
   auto f0 = pack.make_field<0>();
   auto f1 = pack.make_field<1>();
@@ -153,7 +139,8 @@ TEST_F(bitfields_test, pack_field_all) {
   EXPECT_FALSE(f2.all());
 }
 
-TEST_F(bitfields_test, pack_field_any) {
+void pack_field_any() {
+  pack_t pack;
   pack.set(0b010110);
   auto f0 = pack.make_field<0>();
   auto f1 = pack.make_field<1>();
@@ -163,7 +150,8 @@ TEST_F(bitfields_test, pack_field_any) {
   EXPECT_TRUE(f2.any());
 }
 
-TEST_F(bitfields_test, pack_field_none) {
+void pack_field_none() {
+  pack_t pack;
   pack.set(0b010110);
   auto f0 = pack.make_field<0>();
   auto f1 = pack.make_field<1>();
@@ -173,13 +161,15 @@ TEST_F(bitfields_test, pack_field_none) {
   EXPECT_FALSE(f2.none());
 }
 
-TEST_F(bitfields_test, pack_field_chain) {
+void pack_field_chain() {
+  pack_t pack;
+  pack.set(0b010101);
   EXPECT_TRUE((pack.make_field<0>().all()));
   EXPECT_TRUE((pack.make_field<1>().test(i1)));
   EXPECT_TRUE((pack.make_field<2>().reset(i1).none()));
 }
 
-TEST_F(bitfields_test, pack_field_eq) {
+void pack_field_eq() {
   au::Pack<1, 2, 1, 2> pack;
   pack.set(0b010101);
   auto field = pack.make_field<0>();
@@ -189,7 +179,7 @@ TEST_F(bitfields_test, pack_field_eq) {
   EXPECT_TRUE(field == pack.make_field<2>());
 }
 
-TEST_F(bitfields_test, pack_field_less) {
+void pack_field_less() {
   au::Pack<1, 2, 1, 2> pack;
   pack.set(0b010101);
   auto field = pack.make_field<1>();
@@ -198,13 +188,35 @@ TEST_F(bitfields_test, pack_field_less) {
   EXPECT_TRUE(field < pack.make_field<3>());
 }
 
-TEST_F(bitfields_test, pack_set) {
+void pack_set() {
+  pack_t pack;
+  pack.set(0b010101);
   pack.set<field_t>(i0);
   EXPECT_EQ(0b010111, pack.get());
 }
 
-TEST_F(bitfields_test, pack_reset) {
+void pack_reset() {
+  pack_t pack;
+  pack.set(0b010101);
   pack.reset<field_t>(i1);
   EXPECT_EQ(0b010001, pack.get());
 }
+
+int main() {
+  pack_make_single_field();
+  pack_make_many_field();
+  pack_field_value();
+  pack_field_test();
+  pack_field_set();
+  pack_field_reset();
+  pack_field_all();
+  pack_field_any();
+  pack_field_none();
+  pack_field_chain();
+  pack_field_eq();
+  pack_field_less();
+  pack_set();
+  pack_reset();
+
+  return 0;
 }
