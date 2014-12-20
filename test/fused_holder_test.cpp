03f591542579ba93128ed6ca25512cf5123c77d7
Karol Wozniak <attu.git@email.com>
2014-12-20 18:28:26 +0100

    canAssignValueToHolder

--- test/fused_holder_test.cpp
+++ test/fused_holder_test.cpp
@@ -1,19 +1,28 @@
 #include <archie/utils/fused/holder.h>
-#include <memory>
+#include <test/assert.h>
 
 namespace fused = archie::utils::fused;
 
-struct type_ {};
+struct type_ {
+  type_() = default;
+  template <typename Tp>
+  explicit type_(Tp t)
+      : v{t} {}
+  unsigned v = 0;
+};
 
-bool operator==(type_ const& lhs, type_ const& rhs) { return &lhs == &rhs; }
-
-GENERATE_HOLDER(TypeHolder, type_);
+bool operator==(type_ const& lhs, type_ const& rhs) { return lhs.v == rhs.v; }
 
 void canCreateHolder() {
-  auto h1 = TypeHolder{};
-  auto h2 = fused::holder<std::unique_ptr<type_>>{};
-  (void)h1;
-  (void)h2;
+  auto h = fused::holder<type_>{};
+  EXPECT_EQ(0u, static_cast<type_ const&>(h).v);
+}
+
+void canAssignValueToHolder() {
+  auto h = fused::holder<type_>{3u};
+  EXPECT_EQ(3u, static_cast<type_ const&>(h).v);
+  h = type_{5u};
+  EXPECT_EQ(5u, static_cast<type_ const&>(h).v);
 }
 
 int main() {
