77b271894ec659a70ce41918d942b1a2525cbede
Karol Wozniak <attu.git@email.com>
2014-12-21 21:41:43 +0100

    is_assignable.h

--- include/archie/utils/traits.h
+++ include/archie/utils/traits.h
@@ -2,6 +2,6 @@
 #define ARCHIE_UTILS_TRAITS_H_INCLUDED
 
 #include <archie/utils/traits/is_callable.h>
-#include <archie/utils/traits/is_copy_assignable.h>
+#include <archie/utils/traits/is_assignable.h>
 
 #endif
