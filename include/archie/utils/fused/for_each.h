e98bfe0f32347cc864aa079d8b5d53cbc9c4322e
Karol Wozniak <attu.git@email.com>
2014-12-22 12:08:22 +0100

    use eat in for_each

--- include/archie/utils/fused/for_each.h
+++ include/archie/utils/fused/for_each.h
@@ -2,7 +2,7 @@
 #define ARCHIE_UTILS_FUSED_FOR_EACH_H_INCLUDED
 
 #include <utility>
-#include <archie/utils/meta/alias.h>
+#include <archie/utils/meta/eat.h>
 
 namespace archie {
 namespace utils {
@@ -10,8 +10,7 @@ namespace utils {
 
     template <typename Func, typename... Args>
     decltype(auto) for_each(Func&& func, Args&&... args) {
-      (void)meta::alias<int[]>{0, (func(std::forward<Args>(args)), 0)...};
-      // return std::forward<Func>(func);
+      meta::eat<void>{(func(std::forward<Args>(args)), 0)...};
       return func;
     }
   }
