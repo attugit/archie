51e66ec92bb25faeb8b6528b81cbccfde78b6392
Karol Wozniak <attu.git@email.com>
2014-12-29 00:58:58 +0100

    meta::append

--- test/zip_test.cpp
+++ test/zip_test.cpp
@@ -3,6 +3,17 @@
 namespace archie {
 namespace utils {
   namespace meta {
+
+    template <typename Tp, typename Up>
+    struct append {
+      using type = type_list<Tp, Up>;
+    };
+
+    template <typename Tp, typename... Us>
+    struct append<type_list<Us...>, Tp> {
+      using type = type_list<Us..., Tp>;
+    };
+
     template <typename...>
     struct zip;
 
@@ -22,6 +33,15 @@ namespace meta = archie::utils::meta;
 using meta::type_list;
 
 #include <type_traits>
+
+static_assert(
+    std::is_same<meta::append<int, char>::type, type_list<int, char>>::value,
+    "");
+
+static_assert(std::is_same<meta::append<type_list<int, char>, unsigned>::type,
+                           type_list<int, char, unsigned>>::value,
+              "");
+
 static_assert(std::is_same<meta::zip<type_list<int, char>,
                                      type_list<unsigned, unsigned char>>::type,
                            type_list<type_list<int, unsigned>,
