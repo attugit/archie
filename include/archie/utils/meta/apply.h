cd9c11aa987784cbb42a0a8bfbf7a19edc2edb91
Karol Wozniak <attu.git@email.com>
2015-01-12 20:06:42 +0100

    meta::apply specialization for type_list

--- include/archie/utils/meta/apply.h
+++ include/archie/utils/meta/apply.h
@@ -1,18 +1,21 @@
 #ifndef ARCHIE_UTILS_META_APPLY_H_INCLUDED
 #define ARCHIE_UTILS_META_APPLY_H_INCLUDED
 
-#include <utility>
+#include <archie/utils/meta/type_list.h>
 
 namespace archie {
 namespace utils {
   namespace meta {
-    template <template <typename...> class MetaFunction, typename... Args>
+    template <template <typename...> class F, typename... Args>
     struct apply {
-      using type = typename MetaFunction<Args...>::type;
+      using type = typename F<Args...>::type;
     };
 
-    template <template <typename...> class MetaFunction, typename... Args>
-    using apply_t = typename apply<MetaFunction, Args...>::type;
+    template <template <typename...> class F, typename... Args>
+    struct apply<F, type_list<Args...>> : apply<F, Args...> {};
+
+    template <template <typename...> class F, typename... Args>
+    using apply_t = typename apply<F, Args...>::type;
   }
 }
 }
