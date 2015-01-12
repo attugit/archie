aa5ab45f46576fe6ff32bf91d1daac7171d2c68e
Karol Wozniak <attu.git@email.com>
2015-01-12 20:38:53 +0100

    meta::apply test

--- test/meta_algorithm_test.cpp
+++ test/meta_algorithm_test.cpp
@@ -1,5 +1,6 @@
 #include <archie/utils/meta/returns.h>
 #include <archie/utils/meta/transform.h>
+#include <archie/utils/meta/apply.h>
 #include <type_traits>
 
 namespace meta = archie::utils::meta;
@@ -33,6 +34,25 @@ static_assert(
         meta::transform_t<make_wrapper, meta::type_list<int, char, int>>,
         meta::type_list<wrapper<int>, wrapper<char>, wrapper<int>>>::value,
     "");
+}
+
+namespace {
+
+template <typename... Ts>
+struct vsize {
+  using type = std::integral_constant<unsigned, sizeof...(Ts)>;
+};
+
+static_assert(meta::apply_t<vsize>::value == 0, "");
+static_assert(meta::apply_t<vsize, int>::value == 1, "");
+static_assert(meta::apply_t<vsize, int, char>::value == 2, "");
+static_assert(meta::apply_t<vsize, int, char, int>::value == 3, "");
+
+static_assert(meta::apply_t<vsize, meta::type_list<>>::value == 0, "");
+static_assert(meta::apply_t<vsize, meta::type_list<int>>::value == 1, "");
+static_assert(meta::apply_t<vsize, meta::type_list<int, char>>::value == 2, "");
+static_assert(meta::apply_t<vsize, meta::type_list<int, char, int>>::value == 3,
+              "");
 }
 
 int main() { return 0; }
