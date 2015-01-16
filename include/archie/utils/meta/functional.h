848d31bc9fc37db7e55ae82fe7c14e1c89832f38
Karol Wozniak <attu.git@email.com>
2015-01-16 08:25:47 +0100

    meta::less_t and meta::greater_t

--- include/archie/utils/meta/functional.h
+++ include/archie/utils/meta/functional.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <archie/utils/meta/boolean.h>
 
 namespace archie {
 namespace utils {
@@ -8,6 +9,12 @@ namespace utils {
 
     template <typename Condition, typename Tp, typename Up>
     using if_t = typename std::conditional<Condition::value, Tp, Up>::type;
+
+    template <typename Lhs, typename Rhs>
+    using less_t = if_t<boolean<(Lhs::value < Rhs::value)>, true_t, false_t>;
+
+    template <typename Lhs, typename Rhs>
+    using greater_t = less_t<Rhs, Lhs>;
   }
 }
 }
--- test/meta_functional_test.cpp
+++ test/meta_functional_test.cpp
@@ -1,5 +1,5 @@
 #include <archie/utils/meta/functional.h>
-#include <archie/utils/meta/boolean.h>
+#include <archie/utils/meta/number.h>
 
 namespace meta = archie::utils::meta;
 
@@ -9,6 +9,16 @@ static_assert(std::is_same<int, meta::if_t<meta::true_t, int, char>>::value,
               "");
 static_assert(std::is_same<char, meta::if_t<meta::false_t, int, char>>::value,
               "");
+}
+
+namespace {
+static_assert(meta::less_t<meta::number<1>, meta::number<2>>::value, "");
+static_assert(!meta::less_t<meta::number<2>, meta::number<1>>::value, "");
+}
+
+namespace {
+static_assert(!meta::greater_t<meta::number<1>, meta::number<2>>::value, "");
+static_assert(meta::greater_t<meta::number<2>, meta::number<1>>::value, "");
 }
 
 int main() { return 0; }
