f0904fb6004dd0128ada80820e57b2681878bc42
Karol Wozniak <attu.git@email.com>
2015-01-16 11:01:17 +0100

    use meta::eval in meta::apply

--- include/archie/utils/meta/apply.h
+++ include/archie/utils/meta/apply.h
@@ -1,5 +1,4 @@
-#ifndef ARCHIE_UTILS_META_APPLY_H_INCLUDED
-#define ARCHIE_UTILS_META_APPLY_H_INCLUDED
+#pragma once
 
 #include <archie/utils/meta/type_list.h>
 #include <archie/utils/meta/returns.h>
@@ -8,15 +7,13 @@ namespace archie {
 namespace utils {
   namespace meta {
     template <template <typename...> class F, typename... Args>
-    struct apply : returns<typename F<Args...>::type> {};
+    struct apply : returns<eval<F<Args...>>> {};
 
     template <template <typename...> class F, typename... Args>
     struct apply<F, type_list<Args...>> : apply<F, Args...> {};
 
     template <template <typename...> class F, typename... Args>
-    using apply_t = typename apply<F, Args...>::type;
+    using apply_t = eval<apply<F, Args...>>;
   }
 }
 }
-
-#endif
