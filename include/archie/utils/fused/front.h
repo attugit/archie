2197515bc93c6c9947ca75d9df23a244ecf80dbb
Karol Wozniak <attu.git@email.com>
2015-01-19 14:03:15 +0100

    fix meta::front

--- include/archie/utils/fused/front.h
+++ include/archie/utils/fused/front.h
@@ -6,8 +6,8 @@ namespace archie {
 namespace utils {
   namespace fused {
 
-    template <typename Tp>
-    constexpr decltype(auto) front(Tp&& t, ...) noexcept {
+    template <typename Tp, typename... Us>
+    constexpr decltype(auto) front(Tp&& t, Us&&...) noexcept {
       return std::forward<Tp>(t);
     }
   }
--- test/front_test.cpp
+++ test/front_test.cpp
@@ -8,6 +8,13 @@ void canUseFrontWithRValue() {
   EXPECT_EQ(1u, a);
 }
 
+#include <memory>
+
+void canPassNoncopyableToFront() {
+  auto a = fused::front(1u, 2.0, std::make_unique<char>('c'));
+  EXPECT_EQ(1u, a);
+}
+
 void canUseBackWithLValue() {
   unsigned a = 1u;
   auto const& x = fused::front(a, 2.0, '3');
