cbb0a70b255cf440cadeb4b6d31f8ed4e8270c85
Karol Wozniak <attu.git@email.com>
2015-01-19 21:46:57 +0100

    move implementation to placeholder

--- include/archie/utils/fused/nth.h
+++ include/archie/utils/fused/nth.h
@@ -1,28 +1,15 @@
 #pragma once
 
 #include <utility>
-#include <archie/utils/meta/ignore.h>
-#include <archie/utils/meta/number.h>
-#include <archie/utils/meta/indexable.h>
+#include <archie/utils/fused/placeholder.h>
 
 namespace archie {
 namespace utils {
   namespace fused {
-    namespace detail {
-      template <std::size_t... other>
-      struct nth {
-        template <typename Tp, typename... Us>
-        constexpr auto operator()(meta::eat<meta::number<other>>..., Tp&& x,
-                                  Us&&...) noexcept -> decltype(x) {
-          return std::forward<Tp>(x);
-        }
-      };
-    }
-
     template <std::size_t n, typename... Ts>
     decltype(auto) nth(Ts&&... args) {
       static_assert(n < sizeof...(Ts), "");
-      return meta::indexable_t<detail::nth, n>{}(std::forward<Ts>(args)...);
+      return placeholder<n>{}(std::forward<Ts>(args)...);
     }
   }
 }
--- include/archie/utils/fused/placeholder.h
+++ include/archie/utils/fused/placeholder.h
@@ -1,14 +1,26 @@
 #pragma once
 
-#include <archie/utils/fused/nth.h>
+#include <utility>
+#include <archie/utils/meta/ignore.h>
+#include <archie/utils/meta/number.h>
 #include <archie/utils/meta/indexable.h>
 
 namespace archie {
 namespace utils {
   namespace fused {
+    namespace detail {
+      template <std::size_t... other>
+      struct placeholder {
+        template <typename Tp, typename... Us>
+        constexpr auto operator()(meta::eat<meta::number<other>>..., Tp&& x,
+                                  Us&&...) noexcept -> decltype(x) {
+          return std::forward<Tp>(x);
+        }
+      };
+    }
 
     template <std::size_t n>
-    struct placeholder : meta::indexable_t<detail::nth, n> {};
+    struct placeholder : meta::indexable_t<detail::placeholder, n> {};
 
     template <typename F, std::size_t... ids>
     decltype(auto) reorder(F f, placeholder<ids>... xs) {
