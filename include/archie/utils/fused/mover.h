4e6366d67adf87d0a4fedb7cd90cee0b471e16c8
Karol Wozniak <attu.git@email.com>
2015-01-30 02:14:00 +0100

    rename mover to move_capture

--- include/archie/utils/fused/mover.h
+++ include/archie/utils/fused/mover.h
@@ -8,19 +8,22 @@
 namespace archie {
 namespace utils {
   namespace fused {
-    template <typename Tp>
-    struct mover {
-      using value_type = std::decay_t<Tp>;
-      using reference = value_type&;
-      template <typename Up>
-      mover(Up&& u)
-          : value(std::forward<Up>(u)) {}
-      mover(mover&&) = default;
-      mover(mover const&) = default;
-      mover(mover& other) : value(std::forward<Tp>(other.value)) {}
-      operator reference() { return value; }
-      Tp value;
-    };
+    namespace detail {
+      template <typename Tp>
+      struct move_capture {
+        using value_type = std::decay_t<Tp>;
+        using reference = value_type&;
+        template <typename Up>
+        move_capture(Up&& u)
+            : value(std::forward<Up>(u)) {}
+        move_capture(move_capture&&) = default;
+        move_capture(move_capture const&) = default;
+        move_capture(move_capture& other)
+            : value(std::forward<Tp>(other.value)) {}
+        operator reference() { return value; }
+        Tp value;
+      };
+    }
 
     template <typename Tp>
     using move_t = meta::if_t<
@@ -29,7 +32,7 @@ namespace utils {
                             meta::opposite_t<std::is_copy_constructible<Tp>>,
                             meta::opposite_t<
                                 traits::is_trivially_copy_constructible<Tp>>>>,
-        mover<Tp>, Tp>;
+        detail::move_capture<Tp>, Tp>;
   }
 }
 }
