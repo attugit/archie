3e52320e54122c8650ca9f96a9fe15c619eff4d5
Karol Wozniak <attu.git@email.com>
2015-01-31 23:52:29 +0100

    fused::concat two tuples

--- include/archie/utils/fused/concat.h
+++ include/archie/utils/fused/concat.h
@@ -1,16 +1,40 @@
 #pragma once
 
 #include <archie/utils/fused/tuple.h>
+#include <archie/utils/traits/is_fused_tuple.h>
+#include <archie/utils/meta/boolean.h>
 
 namespace archie {
 namespace utils {
   namespace fused {
     namespace detail {
       struct concat {
+      private:
+        template <typename Vp, typename... Ts, typename... Us>
+        constexpr decltype(auto) eval(meta::boolean<false>,
+                                      tuple<Ts...> const& t, Vp&& v,
+                                      Us&&... us) const {
+          return make_tuple(get<Ts>(t)..., std::forward<Vp>(v),
+                            std::forward<Us>(us)...);
+        }
         template <typename... Ts, typename... Us>
-        constexpr decltype(auto) operator()(tuple<Ts...> const& t,
-                                            Us&&... us) const {
-          return make_tuple(get<Ts>(t)..., std::forward<Us>(us)...);
+        constexpr decltype(auto) eval(meta::boolean<true>,
+                                      tuple<Ts...> const& t,
+                                      tuple<Us...> const& u) const {
+          return make_tuple(get<Ts>(t)..., get<Us>(u)...);
+        }
+
+      public:
+        template <typename Tp, typename... Us>
+        constexpr decltype(auto) operator()(tuple<Us...> const& us,
+                                            Tp&& t) const {
+          return eval(traits::is_fused_tuple<Tp>{}, us, std::forward<Tp>(t));
+        }
+        template <typename V1, typename V2, typename... Ts, typename... Us>
+        constexpr decltype(auto) operator()(tuple<Ts...> const& t, V1&& v1,
+                                            V2&& v2, Us&&... us) const {
+          return eval(meta::boolean<false>{}, t, std::forward<V1>(v1),
+                      std::forward<V2>(v2), std::forward<Us>(us)...);
         }
       };
     }
--- test/fused_concat_test.cpp
+++ test/fused_concat_test.cpp
@@ -4,14 +4,42 @@
 namespace fused = archie::utils::fused;
 
 void canConcatTuple() {
-  auto a = fused::make_tuple(1, 2u, '3');
-  auto x = fused::concat(a, 4.0);
+  {
+    auto a = fused::make_tuple(1, 2u, '3');
+    auto x = fused::concat(a, 4.0);
 
-  EXPECT_EQ(4u, fused::tuple_size<decltype(x)>::value);
+    EXPECT_EQ(4u, fused::tuple_size<decltype(x)>::value);
+    EXPECT_EQ(1, fused::get<0>(x));
+    EXPECT_EQ(2u, fused::get<1>(x));
+    EXPECT_EQ('3', fused::get<2>(x));
+    EXPECT_EQ(4.0, fused::get<3>(x));
+  }
+  {
+    auto a = fused::make_tuple(1, 2u, '3');
+    auto x = fused::concat(a, 4.0, 5, 6u);
+
+    EXPECT_EQ(6u, fused::tuple_size<decltype(x)>::value);
+    EXPECT_EQ(1, fused::get<0>(x));
+    EXPECT_EQ(2u, fused::get<1>(x));
+    EXPECT_EQ('3', fused::get<2>(x));
+    EXPECT_EQ(4.0, fused::get<3>(x));
+    EXPECT_EQ(5, fused::get<4>(x));
+    EXPECT_EQ(6u, fused::get<5>(x));
+  }
+}
+
+void canConcatTwoTuples() {
+  auto a = fused::make_tuple(1, 2u, '3');
+  auto b = fused::make_tuple(4.0, 5, 6u);
+  auto x = fused::concat(a, b);
+
+  EXPECT_EQ(6u, fused::tuple_size<decltype(x)>::value);
   EXPECT_EQ(1, fused::get<0>(x));
   EXPECT_EQ(2u, fused::get<1>(x));
   EXPECT_EQ('3', fused::get<2>(x));
   EXPECT_EQ(4.0, fused::get<3>(x));
+  EXPECT_EQ(5, fused::get<4>(x));
+  EXPECT_EQ(6u, fused::get<5>(x));
 }
 
 int main() {
