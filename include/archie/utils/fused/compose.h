acdecf4bbfc3b3a4f1040d2d13d93130a8613354
Karol Wozniak <attu.git@email.com>
2015-02-04 18:36:16 +0100

    add missing forwards

--- include/archie/utils/fused/compose.h
+++ include/archie/utils/fused/compose.h
@@ -16,23 +16,24 @@ namespace utils {
         template <typename F, typename... Ts>
         constexpr decltype(auto) do_work_t(meta::true_t, F&& f,
                                            Ts&&... args) const {
-          return do_work(meta::false_t{}, fused::get<0>(f),
+          return do_work(meta::false_t{}, fused::get<0>(std::forward<F>(f)),
                          std::forward<Ts>(args)...);
         }
         template <typename Fs, typename... Ts>
         constexpr decltype(auto) do_work_t(meta::false_t, Fs&& fs,
                                            Ts&&... args) const {
-          return fused::apply(fused::get<0>(fs),
-                              do_work(meta::true_t{}, fused::tail(fs),
+          return fused::apply(fused::get<0>(std::forward<Fs>(fs)),
+                              do_work(meta::true_t{},
+                                      fused::tail(std::forward<Fs>(fs)),
                                       std::forward<Ts>(args)...));
         }
         template <typename F, typename... Ts>
         constexpr decltype(auto) do_work(meta::true_t, F&& f,
                                          Ts&&... args) const {
-          using is_single =
-              meta::boolean<fused::tuple_size<std::decay_t<F>>::value == 1>;
-          return do_work_t(meta::if_t<is_single, meta::true_t, meta::false_t>{},
-                           std::forward<F>(f), std::forward<Ts>(args)...);
+          return do_work_t(
+              meta::boolean<(
+                  fused::tuple_size<std::remove_reference_t<F>>::value == 1)>{},
+              std::forward<F>(f), std::forward<Ts>(args)...);
         }
         template <typename F, typename... Ts>
         constexpr decltype(auto) do_work(meta::false_t, F&& f,
