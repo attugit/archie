1b281af2d384b1d254501408d7948afa2dcda299
Karol Wozniak <attu.git@email.com>
2015-02-05 19:53:30 +0100

    more tests for fused::apply

--- test/fused_apply_test.cpp
+++ test/fused_apply_test.cpp
@@ -1,4 +1,5 @@
 #include <archie/utils/fused/apply.h>
+#include <archie/utils/fused/front.h>
 
 auto count = [](auto const&... xs) { return sizeof...(xs); };
 
@@ -28,9 +29,37 @@ void canUseApplyWithRValueTuple() {
   EXPECT_EQ(3u, fused::apply(count, fused::make_tuple(3, 2, 1)));
 }
 
+void canUseApplyWithMakeTuple() {
+  auto x = fused::apply(fused::make_tuple, 1, 2u, '3');
+  static_assert(fused::tuple_size<decltype(x)>::value == 3u, "");
+}
+
+void canUseApplyResultOfAnotherApply() {
+  {
+    auto x =
+        fused::apply(fused::front, fused::apply(fused::make_tuple, 1, 2u, '3'));
+    EXPECT_EQ(1, x);
+  }
+  auto a = 1;
+  auto b = 2u;
+  auto c = '3';
+  {
+    auto x =
+        fused::apply(fused::front, fused::apply(fused::make_tuple, a, b, c));
+    EXPECT_EQ(a, x);
+  }
+  {
+    auto const& x =
+        fused::apply(fused::front, fused::apply(fused::tie, a, b, c));
+    EXPECT_EQ(&a, &x);
+  }
+}
+
 int main() {
   canUseApplyWithRegularArgs();
   canUseApplyWithTuple();
   canUseApplyWithRValueTuple();
+  canUseApplyWithMakeTuple();
+  canUseApplyResultOfAnotherApply();
   return 0;
 }
