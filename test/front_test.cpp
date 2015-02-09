9e8882dd7a0dd7779c1a448e950dcbbd1b6620ff
Karol Wozniak <attu.git@email.com>
2015-02-09 19:58:00 +0100

    additional test for front

--- test/front_test.cpp
+++ test/front_test.cpp
@@ -1,18 +1,29 @@
 #include <archie/utils/fused/front.h>
 #include <archie/utils/test.h>
+#include <memory>
+#include <type_traits>
 
 namespace fused = archie::utils::fused;
+using uptr = std::unique_ptr<unsigned>;
 
 void canUseFrontWithRValue() {
   auto a = fused::front(1u, 2.0, '3');
+  static_assert(std::is_same<decltype(a), unsigned>::value, "");
   EXPECT_EQ(1u, a);
 }
 
-#include <memory>
-
 void canPassNoncopyableToFront() {
-  auto a = fused::front(1u, 2.0, std::make_unique<char>('c'));
-  EXPECT_EQ(1u, a);
+  {
+    auto a = fused::front(1u, 2.0, std::make_unique<char>('c'));
+    static_assert(std::is_same<decltype(a), unsigned>::value, "");
+    EXPECT_EQ(1u, a);
+  }
+  {
+    auto a = fused::front(std::make_unique<unsigned>(1u), 2.0, '3');
+    static_assert(std::is_same<decltype(a), uptr>::value, "");
+    EXPECT_TRUE(a != nullptr);
+    EXPECT_EQ(1u, *a);
+  }
 }
 
 void canUseBackWithLValue() {
