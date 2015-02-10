819ffe844d125832da6ac27f11f2d9a4f8372abf
Karol Wozniak <attu.git@email.com>
2015-02-10 18:40:15 +0100

    fused::take variable_template

--- include/archie/utils/fused/take.h
+++ include/archie/utils/fused/take.h
@@ -4,6 +4,7 @@
 #include <archie/utils/meta/type_list.h>
 #include <archie/utils/meta/take.h>
 #include <archie/utils/meta/indexable.h>
+#include <archie/utils/meta/variable_template.h>
 #include <archie/utils/fused/tuple.h>
 #include <archie/utils/fused/nth.h>
 #include <archie/utils/fused/as_tuple.h>
@@ -19,7 +20,6 @@ namespace utils {
         constexpr decltype(auto) operator()(tuple<Ts...> const& t) const {
           return fused::tuple<meta::at_t<ids, Ts...>...>(get<ids>(t)...);
         }
-#if defined(HAS_VARIABLE_TEMPLATES)
         template <typename... Ts>
         constexpr decltype(auto) operator()(tuple<Ts...>&& t) const {
           return make_tuple(get<ids>(t)...);
@@ -32,16 +32,19 @@ namespace utils {
         constexpr decltype(auto) operator()(Ts&&... ts) const {
           return make_tuple(fused::nth<ids>(ts...)...);
         }
-#endif
       };
     }
 #if defined(HAS_VARIABLE_TEMPLATES)
     template <std::size_t n>
     constexpr meta::indexable_t<detail::take, n> take{};
 #else
+    template <std::size_t n>
+    struct take_v
+        : meta::variable_template<meta::indexable_t<detail::take, n>> {};
+
     template <std::size_t n, typename... Ts>
     decltype(auto) take(tuple<Ts...> const& t) {
-      return meta::indexable_t<detail::take, n>()(t);
+      return fused::take_v<n>::value(t);
     }
 #endif
   }
