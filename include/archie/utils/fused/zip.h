86523117b2bdc5149228643d8881093584a0565a
Karol Wozniak <attu.git@email.com>
2015-02-13 20:56:50 +0100

    keep common impl between fused::zip and fused::zip_view

--- include/archie/utils/fused/zip.h
+++ include/archie/utils/fused/zip.h
@@ -7,31 +7,38 @@ namespace archie {
 namespace utils {
   namespace fused {
     namespace detail {
-      struct zip_ {
-        template <typename... Ts, typename... Us>
-        constexpr decltype(auto) operator()(tuple<Ts...> const& a,
-                                            tuple<Us...> const& b) const {
-          return make_tuple(make_tuple(get<Ts>(a), get<Us>(b))...);
-        }
-      };
-
-      struct zip_view_ {
+      struct zip_impl {
       private:
         template <std::size_t... ns>
         struct impl {
-          template <typename Tp, typename Up>
-          constexpr decltype(auto) operator()(Tp&& a, Up&& b) const {
+          template <typename F, typename Tp, typename Up>
+          constexpr decltype(auto) operator()(F&& f, Tp&& a, Up&& b) const {
             return fused::make_tuple(
-                fused::tie(fused::get<ns>(a), fused::get<ns>(b))...);
+                std::forward<F>(f)(fused::get<ns>(a), fused::get<ns>(b))...);
           }
         };
-
       public:
-        template <typename Tp, typename Up>
-        constexpr decltype(auto) operator()(Tp&& a, Up&& b) const {
+        template <typename F, typename Tp, typename Up>
+        constexpr decltype(auto) operator()(F&& f, Tp&& a, Up&& b) const {
           return meta::indexable_t<
               impl, fused::tuple_size<std::remove_reference_t<Tp>>::value>{}(
-              std::forward<Tp>(a), std::forward<Up>(b));
+              std::forward<F>(f), std::forward<Tp>(a), std::forward<Up>(b));
+        }
+      };
+
+      struct zip_ : private zip_impl {
+        template <typename Tp, typename Up>
+        constexpr decltype(auto) operator()(Tp&& a, Up&& b) const {
+          return zip_impl::operator()(fused::make_tuple, std::forward<Tp>(a),
+                                      std::forward<Up>(b));
+        }
+      };
+
+      struct zip_view_ : private zip_impl {
+        template <typename Tp, typename Up>
+        constexpr decltype(auto) operator()(Tp&& a, Up&& b) const {
+          return zip_impl::operator()(fused::tie, std::forward<Tp>(a),
+                                      std::forward<Up>(b));
         }
       };
     }
