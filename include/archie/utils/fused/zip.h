9dacddb979ec646480b2e4ec2528fda9feb33edc
Karol Wozniak <attu.git@email.com>
2015-02-13 07:18:44 +0100

    fused::zip_view

--- include/archie/utils/fused/zip.h
+++ include/archie/utils/fused/zip.h
@@ -13,8 +13,17 @@ namespace utils {
           return make_tuple(make_tuple(get<Ts>(a), get<Us>(b))...);
         }
       };
+
+      struct zip_view_ {
+        template <typename... Ts, typename... Us>
+        constexpr decltype(auto) operator()(tuple<Ts...>& a,
+                                            tuple<Us...>& b) const {
+          return make_tuple(tie(get<Ts>(a), get<Us>(b))...);
+        }
+      };
     }
     constexpr detail::zip_ zip{};
+    constexpr detail::zip_view_ zip_view{};
   }
 }
 }
--- test/fused_zip_test.cpp
+++ test/fused_zip_test.cpp
@@ -14,7 +14,27 @@ void canZipTwoTuples() {
   EXPECT_EQ(fused::make_tuple('3', 6u), fused::get<2>(x));
 }
 
+void canZipViewTwoTuples() {
+  auto a = fused::make_tuple(1, 2u, '3');
+  auto b = fused::make_tuple('4', 5.0, 6u);
+  auto x = fused::zip_view(a, b);
+
+  EXPECT_EQ(3u, fused::tuple_size<decltype(x)>::value);
+  auto const& x0 = fused::get<0>(x);
+  auto const& x1 = fused::get<1>(x);
+  auto const& x2 = fused::get<2>(x);
+
+  EXPECT_EQ(&fused::get<0>(a), &fused::get<0>(x0));
+  EXPECT_EQ(&fused::get<1>(a), &fused::get<0>(x1));
+  EXPECT_EQ(&fused::get<2>(a), &fused::get<0>(x2));
+
+  EXPECT_EQ(&fused::get<0>(b), &fused::get<1>(x0));
+  EXPECT_EQ(&fused::get<1>(b), &fused::get<1>(x1));
+  EXPECT_EQ(&fused::get<2>(b), &fused::get<1>(x2));
+}
+
 int main() {
   canZipTwoTuples();
+  canZipViewTwoTuples();
   return 0;
 }
