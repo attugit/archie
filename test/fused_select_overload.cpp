469895274f8b82dda8481aea9548e8bbd438039b
Karol Wozniak <attu.git@email.com>
2015-02-15 15:03:33 +0100

    more testes for fused::select_overload

--- test/fused_select_overload.cpp
+++ test/fused_select_overload.cpp
@@ -1,8 +1,11 @@
 #include <archie/utils/fused/select_overload.h>
+#include <archie/utils/meta/number.h>
 #include <archie/utils/test.h>
 #include <utility>
+#include <type_traits>
 
 namespace fused = archie::utils::fused;
+namespace meta = archie::utils::meta;
 
 template <unsigned N, typename = typename std::enable_if<(N <= 8)>::type>
 constexpr decltype(auto) foo(fused::choice<1>) {
@@ -29,7 +32,38 @@ void canSelectOverload() {
   EXPECT_EQ(11u, z);
 }
 
+struct overloaded {
+  template <typename Tp>
+  constexpr decltype(auto) operator()(Tp&& t) const {
+    return impl<Tp>(fused::select_overload, std::forward<Tp>(t));
+  }
+
+private:
+  template <typename Tp,
+            typename = typename std::enable_if<std::is_signed<Tp>::value>::type>
+  constexpr decltype(auto) impl(fused::choice<1>, Tp&&) const {
+    return meta::number<sizeof(Tp)>{};
+  }
+  template <typename Tp, typename = typename std::enable_if<
+                             std::is_fundamental<Tp>::value>::type>
+  constexpr decltype(auto) impl(fused::choice<2>, Tp&& t) const {
+    return std::forward<Tp>(t);
+  }
+  template <typename Tp>
+  constexpr decltype(auto) impl(fused::otherwise, Tp&&) const {
+    return 0u;
+  }
+};
+
+void canSelectOverloadMember() {
+  constexpr overloaded over{};
+  EXPECT_EQ(4u, over(1));
+  EXPECT_EQ(7u, over(7u));
+  EXPECT_EQ(0u, over(meta::number<7u>{}));
+}
+
 int main() {
   canSelectOverload();
+  canSelectOverloadMember();
   return 0;
 }
