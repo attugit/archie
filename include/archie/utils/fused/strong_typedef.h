470f903b6cb0e0926e29932268b902bbd13fd6f6
Karol Wozniak <attu.git@email.com>
2015-02-23 19:33:42 +0100

    simplify policies

--- include/archie/utils/fused/strong_typedef.h
+++ include/archie/utils/fused/strong_typedef.h
@@ -15,28 +15,12 @@ namespace utils {
     strong_typedef<Ts...>& copy_assing(strong_typedef<Ts...>&, Up&&);
 
     inline namespace policy {
+      template <typename>
+      struct eq {};
+      template <typename>
+      struct lt {};
       template <typename Tp>
-      struct eq {
-        template <typename Up, typename Vp>
-        bool eval(Up const& u, Vp const& v) const {
-          return static_cast<Tp const&>(u) == static_cast<Tp const&>(v);
-        }
-      };
-      template <typename Tp>
-      struct lt {
-        template <typename Up, typename Vp>
-        bool eval(Up const& u, Vp const& v) const {
-          return static_cast<Tp const&>(u) < static_cast<Tp const&>(v);
-        }
-      };
-      template <typename Tp>
-      struct add {
-        template <typename Up, typename Vp>
-        Up& eval(Up& u, Vp const& v) const {
-          return copy_assing(u, static_cast<Tp const&>(u) +
-                                    static_cast<Tp const&>(v));
-        }
-      };
+      struct add {};
 
       template <typename Tp, typename... Ps>
       using has = meta::requires_all<std::is_base_of<Ps, Tp>...>;
@@ -68,8 +52,8 @@ namespace utils {
       }
 
       template <typename Up, typename = policy::has<type, eq<Up>>>
-      friend bool operator==(type const& s, Up const& u) {
-        return s.eq<Up>::eval(s, u);
+      friend bool operator==(type const& t, Up const& u) {
+        return static_cast<Tp const&>(t) == static_cast<Tp const&>(u);
       }
       template <typename Up, typename = policy::has<type, eq<Up>>>
       friend bool operator==(Up const& u, type const& s) {
@@ -85,11 +69,11 @@ namespace utils {
       }
       template <typename Up, typename = policy::has<type, lt<Up>>>
       friend bool operator<(type const& s, Up const& u) {
-        return s.lt<Up>::eval(s, u);
+        return static_cast<Tp const&>(s) < static_cast<Tp const&>(u);
       }
       template <typename Up, typename = policy::has<type, lt<Up>>>
       friend bool operator<(Up const& u, type const& s) {
-        return s.lt<Up>::eval(u, s);
+        return static_cast<Tp const&>(u) < static_cast<Tp const&>(s);
       }
       template <typename Up, typename = policy::has<type, lt<Up>>>
       friend bool operator>(type const& s, Up const& u) {
@@ -116,8 +100,9 @@ namespace utils {
         return !(u < s);
       }
       template <typename Up, typename = policy::has<type, add<Up>>>
-      friend type& operator+=(type& s, Up const& u) {
-        return s.add<Up>::eval(s, u);
+      friend type& operator+=(type& t, Up const& u) {
+        return copy_assing(t, static_cast<Tp const&>(t) +
+                                  static_cast<Tp const&>(u));
       }
       template <typename Up, typename = policy::has<type, add<Up>>>
       friend type operator+(type const& t, Up const& u) {
