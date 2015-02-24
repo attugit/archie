c5bec7232dfa50b1c9ae80953551e6f6ccb0224a
Karol Wozniak <attu.git@email.com>
2015-02-24 19:43:40 +0100

    replace fused::holder with fused::strong_typedef

--- test/aggregates_test.cpp
+++ test/aggregates_test.cpp
@@ -1,33 +1,29 @@
-#include <archie/utils/fused/holder.h>
+#include <archie/utils/fused/strong_typedef.h>
+#include <archie/utils/fused/policies.h>
 #include <archie/utils/fused/tuple.h>
 #include <archie/utils/fused/tuple_view.h>
 #include <string>
 #include <archie/utils/test.h>
 
+namespace fused = archie::utils::fused;
+
 namespace detail {
 struct PackedStruct {
-  struct Id : archie::utils::fused::holder<unsigned> {
-    using holder::holder;
-    Id(Id const&) = default;
-    Id(Id&) = default;
+  struct Id : fused::strong_typedef<Id, unsigned, fused::policy::eq<unsigned>> {
+    using self_t::self_t;
   };
-  struct Name : archie::utils::fused::holder<std::string> {
-    using holder::holder;
-    Name(Name const&) = default;
-    Name(Name&) = default;
+  struct Name : fused::strong_typedef<Name, std::string,
+                                      fused::policy::eq<std::string>> {
+    using self_t::self_t;
   };
-  struct Value : archie::utils::fused::holder<int> {
-    using holder::holder;
-    Value(Value const&) = default;
-    Value(Value&) = default;
+  struct Value : fused::strong_typedef<Value, int, fused::policy::eq<int>> {
+    using self_t::self_t;
   };
-  struct Amount : archie::utils::fused::holder<unsigned> {
-    using holder::holder;
-    Amount(Amount const&) = default;
-    Amount(Amount&) = default;
+  struct Amount : fused::strong_typedef<Amount, unsigned> {
+    using self_t::self_t;
   };
 
-  using type = archie::utils::fused::tuple<Id, Name, Value, Amount>;
+  using type = fused::tuple<Id, Name, Value, Amount>;
 };
 }
 
@@ -36,19 +32,16 @@ using Id = detail::PackedStruct::Id;
 using Name = detail::PackedStruct::Name;
 using Value = detail::PackedStruct::Value;
 using Amount = detail::PackedStruct::Amount;
-using BaseType = detail::PackedStruct::type;
-
-namespace fused = archie::utils::fused;
 
 void canCreate() {
-  PackedStruct pack{Id{1}, Name{"Aqq"}, Value{-20}, Amount{10}};
+  PackedStruct pack{Id{1u}, Name{"Aqq"}, Value{-20}, Amount{10u}};
   EXPECT_EQ(1u, fused::get<Id>(pack));
-  EXPECT_EQ("Aqq", fused::get<Name>(pack));
+  EXPECT_EQ(std::string{"Aqq"}, fused::get<Name>(pack));
   EXPECT_EQ(-20, fused::get<Value>(pack));
 }
 
 void canSelect() {
-  PackedStruct pack{Id{1}, Name{"Aqq"}, Value{-20}, Amount{10}};
+  PackedStruct pack{Id{1u}, Name{"Aqq"}, Value{-20}, Amount{10u}};
   auto select = fused::select<Id, Value>(pack);
   EXPECT_EQ(1u, fused::get<Id>(select));
   EXPECT_EQ(-20, fused::get<Value>(select));
