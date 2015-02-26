658b60304953cb12891979b2eb524a7bc7dd7036
Karol Wozniak <attu.git@email.com>
2015-02-26 23:24:34 +0100

    use ignore

--- test/meta_model_of.cpp
+++ test/meta_model_of.cpp
@@ -1,9 +1,13 @@
 #include <archie/utils/meta/model_of.h>
 #include <archie/utils/meta/requires.h>
+#include <archie/utils/fused/ignore.h>
 #include <archie/utils/test.h>
 
 using archie::utils::meta::requires;
 using archie::utils::meta::model_of;
+
+namespace meta = archie::utils::meta;
+namespace fused = archie::utils::fused;
 
 struct Callable {
   template <class F, class... Ts>
@@ -12,11 +16,11 @@ struct Callable {
 
 struct foo {
   template <typename Tp>
-  int func(Tp, requires<model_of<Callable(Tp)>>* = nullptr) const {
+  int func(Tp, requires<model_of<Callable(Tp)>> = fused::ignore) const {
     return 0;
   }
   template <typename Tp>
-  int func(Tp, requires<model_of<Callable(Tp, int)>>* = nullptr) const {
+  int func(Tp, requires<model_of<Callable(Tp, int)>> = fused::ignore) const {
     return 1;
   }
 };
