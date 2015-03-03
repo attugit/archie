89dccb5f1e54e95648d06c48f6c869a86a64f2e5
Karol Wozniak <attu.git@email.com>
2015-03-03 19:36:58 +0100

    fused::compose implemtation done with conditional

--- include/archie/utils/fused/compose.h
+++ include/archie/utils/fused/compose.h
@@ -1,56 +1,55 @@
 #pragma once
 
 #include <utility>
-#include <archie/utils/meta/functional.h>
 #include <archie/utils/fused/tuple.h>
 #include <archie/utils/fused/apply.h>
 #include <archie/utils/fused/tail.h>
 #include <archie/utils/fused/boolean.h>
+#include <archie/utils/fused/conditional.h>
 #include <archie/utils/traits/is_fused_tuple.h>
 
 namespace archie {
 namespace utils {
   namespace fused {
     namespace detail {
-      struct compose_ {
-      private:
-        template <typename F, typename... Ts>
-        constexpr decltype(auto) do_work_t(decltype(True), F&& f,
-                                           Ts&&... args) const {
-          return do_work(False, fused::get<0>(std::forward<F>(f)),
-                         std::forward<Ts>(args)...);
+      struct compose_args_ {
+        template <typename F, typename... Ts,
+                  typename = meta::requires<
+                      meta::model_of<models::Callable(F, Ts&&...)>>>
+        constexpr decltype(auto) operator()(F&& f, Ts&&... ts) const {
+          return fused::apply(std::forward<F>(f), std::forward<Ts>(ts)...);
         }
-        template <typename Fs, typename... Ts>
-        constexpr decltype(auto) do_work_t(decltype(False), Fs&& fs,
-                                           Ts&&... args) const {
-          return fused::apply(fused::get<0>(std::forward<Fs>(fs)),
-                              do_work(True, fused::tail(std::forward<Fs>(fs)),
-                                      std::forward<Ts>(args)...));
-        }
-        template <typename F, typename... Ts>
-        constexpr decltype(auto) do_work(decltype(True), F&& f,
-                                         Ts&&... args) const {
-          return do_work_t(
+      };
+
+      struct compose_tuple_ {
+        template <typename F, typename... Ts,
+                  typename = meta::requires<
+                      traits::is_fused_tuple<std::remove_reference_t<F>>>>
+        constexpr decltype(auto) operator()(F&& f, Ts&&... ts) const {
+          return impl(
               meta::boolean<(
                   fused::tuple_size<std::remove_reference_t<F>>::value == 1)>{},
-              std::forward<F>(f), std::forward<Ts>(args)...);
-        }
-        template <typename F, typename... Ts>
-        constexpr decltype(auto) do_work(decltype(False), F&& f,
-                                         Ts&&... args) const {
-          return fused::apply(std::forward<F>(f), std::forward<Ts>(args)...);
+              std::forward<F>(f), std::forward<Ts>(ts)...);
         }
 
-      public:
+      private:
         template <typename F, typename... Ts>
-        constexpr decltype(auto) operator()(F&& f, Ts&&... args) const {
-          return do_work(traits::is_fused_tuple<F>{}, std::forward<F>(f),
-                         std::forward<Ts>(args)...);
+        constexpr decltype(auto) impl(decltype(True), F&& f, Ts&&... ts) const {
+          return fused::apply(fused::get<0>(std::forward<F>(f)),
+                              std::forward<Ts>(ts)...);
+        }
+        template <typename F, typename... Ts>
+        constexpr decltype(auto) impl(decltype(False), F&& f,
+                                      Ts&&... ts) const {
+          return fused::apply(fused::get<0>(std::forward<F>(f)),
+                              operator()(fused::tail(std::forward<F>(f)),
+                                         std::forward<Ts>(ts)...));
         }
       };
     }
 
-    constexpr auto const compose = detail::compose_{};
+    constexpr auto const compose = fused::make_conditional(
+        detail::compose_tuple_{}, detail::compose_args_{});
   }
 }
 }
