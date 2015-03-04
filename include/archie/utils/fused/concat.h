b1dca4b1847b96cc6e80680c59f92a7f28ac8502
Karol Wozniak <attu.git@email.com>
2015-03-05 00:07:01 +0100

    use fused::conditional in fused::concat

--- include/archie/utils/fused/concat.h
+++ include/archie/utils/fused/concat.h
@@ -2,41 +2,57 @@
 
 #include <archie/utils/fused/tuple.h>
 #include <archie/utils/traits/is_fused_tuple.h>
+#include <archie/utils/meta/requires.h>
 #include <archie/utils/fused/boolean.h>
+#include <archie/utils/meta/as_type_list.h>
+#include <archie/utils/fused/conditional.h>
 
 namespace archie {
 namespace utils {
   namespace fused {
     namespace detail {
-      struct concat_ {
-      private:
-        template <typename Vp, typename... Ts, typename... Us>
-        constexpr decltype(auto) eval(decltype(False), tuple<Ts...> const& t,
-                                      Vp&& v, Us&&... us) const {
-          return make_tuple(get<Ts>(t)..., std::forward<Vp>(v),
-                            std::forward<Us>(us)...);
-        }
-        template <typename... Ts, typename... Us>
-        constexpr decltype(auto) eval(decltype(True), tuple<Ts...> const& t,
-                                      tuple<Us...> const& u) const {
-          return make_tuple(get<Ts>(t)..., get<Us>(u)...);
+      struct concat_tuples_ {
+        template <typename Tp, typename Up,
+                  typename = meta::requires_all<
+                      traits::is_fused_tuple<std::remove_reference_t<Tp>>,
+                      traits::is_fused_tuple<std::remove_reference_t<Up>>>>
+        constexpr decltype(auto) operator()(Tp&& tp, Up&& up) const {
+          return impl(std::forward<Tp>(tp),
+                      meta::as_type_list_t<std::remove_reference_t<Tp>>{},
+                      std::forward<Up>(up),
+                      meta::as_type_list_t<std::remove_reference_t<Up>>{});
         }
 
-      public:
-        template <typename Tp, typename... Us>
-        constexpr decltype(auto) operator()(tuple<Us...> const& us,
-                                            Tp&& t) const {
-          return eval(traits::is_fused_tuple<Tp>{}, us, std::forward<Tp>(t));
+      private:
+        template <typename Tp, typename Up, typename... Ts, typename... Us>
+        constexpr decltype(auto) impl(Tp&& tp, meta::type_list<Ts...>, Up&& up,
+                                      meta::type_list<Us...>) const {
+          return fused::make_tuple(fused::get<Ts>(std::forward<Tp>(tp))...,
+                                   fused::get<Us>(std::forward<Up>(up))...);
         }
-        template <typename V1, typename V2, typename... Ts, typename... Us>
-        constexpr decltype(auto) operator()(tuple<Ts...> const& t, V1&& v1,
-                                            V2&& v2, Us&&... us) const {
-          return eval(False, t, std::forward<V1>(v1), std::forward<V2>(v2),
+      };
+
+      struct append_tuple_ {
+        template <typename Tp, typename... Us,
+                  typename = meta::requires<
+                      traits::is_fused_tuple<std::remove_reference_t<Tp>>>>
+        constexpr decltype(auto) operator()(Tp&& tp, Us&&... us) const {
+          return impl(std::forward<Tp>(tp),
+                      meta::as_type_list_t<std::remove_reference_t<Tp>>{},
                       std::forward<Us>(us)...);
+        }
+
+      private:
+        template <typename Tp, typename... Ts, typename... Us>
+        constexpr decltype(auto) impl(Tp&& tp, meta::type_list<Ts...>,
+                                      Us&&... us) const {
+          return fused::make_tuple(fused::get<Ts>(std::forward<Tp>(tp))...,
+                                   std::forward<Us>(us)...);
         }
       };
     }
-    constexpr detail::concat_ concat{};
+    constexpr auto const concat = fused::make_conditional(
+        detail::concat_tuples_{}, detail::append_tuple_{});
   }
 }
 }
