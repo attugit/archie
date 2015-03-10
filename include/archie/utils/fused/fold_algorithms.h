51b34c4b3ed49d7fa4470de94e4dcaac4ed63493
Karol Wozniak <attu.git@email.com>
2015-03-10 22:50:29 +0100

    fused::max

---
+++ include/archie/utils/fused/fold_algorithms.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <utility>
+#include <archie/utils/fused/fold.h>
+
+namespace archie {
+namespace utils {
+  namespace fused {
+    namespace detail {
+      struct accumulate_ {
+        template <typename Tp, typename Up, typename... Vs>
+        constexpr decltype(auto) operator()(Tp&& t, Up&& u, Vs&&... vs) const {
+          return fused::make_fold([](auto&& a, auto&& b) {
+            return std::forward<decltype(a)>(a) + std::forward<decltype(b)>(b);
+          })(std::forward<Tp>(t), std::forward<Up>(u), std::forward<Vs>(vs)...);
+        }
+      };
+      struct max_ {
+        template <typename Tp, typename Up, typename... Vs>
+        constexpr decltype(auto) operator()(Tp&& t, Up&& u, Vs&&... vs) const {
+          return fused::make_fold([](auto&& a, auto&& b) {
+            return a > b ? std::forward<decltype(a)>(a) : std::forward<decltype(b)>(b);
+          })(std::forward<Tp>(t), std::forward<Up>(u), std::forward<Vs>(vs)...);
+        }
+      };
+    }
+    constexpr auto const accumulate = detail::accumulate_{};
+    constexpr auto const max = detail::max_{};
+  }
+}
+}
--- test/fused_fold_algo_test.cpp
+++ test/fused_fold_algo_test.cpp
@@ -1,4 +1,4 @@
-#include <archie/utils/fused/accumulate.h>
+#include <archie/utils/fused/fold_algorithms.h>
 #include <archie/utils/fused/tuple.h>
 #include <archie/utils/fused/apply.h>
 #include <archie/utils/test.h>
@@ -25,8 +25,30 @@ void canUseAccumulateWithCustomFunctionObject() {
   EXPECT_EQ(10, fused::apply(fused::make_fold(f, 1), t));
 }
 
+void canUseMax() {
+  EXPECT_EQ(1, fused::max(0, 1));
+  EXPECT_EQ(2, fused::max(0, 1, 2));
+  EXPECT_EQ(3, fused::max(0, 3, 2));
+}
+
+void canApplyMax() {
+  auto const t = fused::make_tuple(0, 3, 2);
+  EXPECT_EQ(3, fused::apply(fused::max, t));
+  EXPECT_EQ(4, fused::apply(fused::max, fused::make_tuple(4, 3, 2)));
+}
+
+void canApplyMaxWithCustomFunctionObject() {
+  auto const t = fused::make_tuple(0, 3, 4);
+  auto const f = [](auto a, auto b) { return 2 * a > b ? a : b; };
+  EXPECT_EQ(3, fused::apply(fused::make_fold(f), t));
+  EXPECT_EQ(4, fused::apply(fused::make_fold(f), fused::make_tuple(4, 3, 6)));
+}
+
 int main() {
   canUseAccumulate();
   canApplyAccumulate();
+  canUseAccumulateWithCustomFunctionObject();
+  canUseMax();
+  canApplyMax();
   return 0;
 }
