6530ba3081994cf75e2b50270a6a0c63a72b9cb9
Karol Wozniak <attu.git@email.com>
2015-03-10 20:05:21 +0100

    can apply fold

--- test/fused_fold_test.cpp
+++ test/fused_fold_test.cpp
@@ -1,5 +1,6 @@
 #include <archie/utils/fused/fold.h>
 #include <archie/utils/fused/tuple.h>
+#include <archie/utils/fused/apply.h>
 #include <archie/utils/test.h>
 
 namespace fused = archie::utils::fused;
@@ -10,7 +11,17 @@ void canUseFold() {
   EXPECT_EQ(fused::make_tuple(fused::make_tuple(0, 1), 2), fused::fold(mkt, 0, 1, 2));
 }
 
+void canApplyFold() {
+  auto const mkt = [](auto&& s, auto&& e) { return fused::make_tuple(s, e); };
+  auto const t = fused::make_tuple(0, 1, 2);
+  EXPECT_EQ(
+      fused::make_tuple(fused::make_tuple(0, 1), 2),
+      fused::apply(
+          [&mkt](auto&&... xs) { return fused::fold(mkt, std::forward<decltype(xs)>(xs)...); }, t));
+}
+
 int main() {
   canUseFold();
+  canApplyFold();
   return 0;
 }
