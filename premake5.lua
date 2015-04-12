7bb8e98d6d73717f1c311efdd13fd750fdeedd8a
Karol Wozniak <attu.git@email.com>
2015-04-12 18:42:29 +0200

    premake5 script

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+_ACTION = _ACTION or "gmake"
+
+newoption {
+  trigger = "targetdir",
+  value = "PATH",
+  description = "Choose target directory"
+}
+
+TARGETDIR = _OPTIONS["targetdir"] or "build"
+
+local archie = solution "archie"
+  location ( TARGETDIR )
+  targetdir ( TARGETDIR )
+  configurations { "debug", "release" }
+  language "C++"
+  flags {
+    "C++14",
+    "ExtraWarnings",
+    "FatalWarnings",
+    "NoExceptions",
+    "NoRTTI",
+    "StaticRuntime"}
+  configuration "release"
+    flags { "OptimizeSpeed" }
+  configuration "debug"
+    flags { "Symbols" }
+
+  include "test"
