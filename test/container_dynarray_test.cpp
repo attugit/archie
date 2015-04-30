44fb64d3edcb67aeea36c8ac0170b6272294b919
Karol Wozniak <attu.git@email.com>
2015-04-30 15:37:04 +0200

    simplify copy assigment test

--- test/container_dynarray_test.cpp
+++ test/container_dynarray_test.cpp
@@ -577,60 +577,31 @@ void canMoveSbo() {
 
 void canCopySbo() {
   enum { empty = 0, stack, heap };
-  std::vector<sbo<resource>> const input = {{}, {3, 5}, {3, 5, 7}};
+  enum { src = 0, func };
+  auto const test1 = [](sbo<resource> const& dst) { EXPECT_TRUE(dst.empty()); };
+  auto const test2 = [](sbo<resource> const& dst) {
+    EXPECT_EQ(2u, cont::capacity(dst));
+    EXPECT_EQ(2u, cont::size(dst));
+    EXPECT_EQ(3, dst[0]);
+    EXPECT_EQ(5, dst[1]);
+  };
+  auto const test3 = [](sbo<resource> const& dst) {
+    EXPECT_EQ(3u, cont::capacity(dst));
+    EXPECT_EQ(3u, cont::size(dst));
+    EXPECT_EQ(3, dst[0]);
+    EXPECT_EQ(5, dst[1]);
+    EXPECT_EQ(7, dst[2]);
+  };
+  std::vector<std::pair<sbo<resource>, std::function<void(sbo<resource> const&)>>> const input = {
+      {{}, test1}, {{3, 5}, test2}, {{3, 5, 7}, test3}};
   std::vector<sbo<resource>> const data = {{}, {11, 13}, {17, 19, 23}};
-  {
-    auto const& src = input[empty];
-    auto dst = data;
-    auto const test = [](sbo<resource> const& dst) { EXPECT_TRUE(dst.empty()); };
-    // A1
-    dst[empty] = src;
-    test(dst[empty]);
-    // A4
-    dst[stack] = src;
-    test(dst[stack]);
-    // A7
-    dst[heap] = src;
-    test(dst[heap]);
-  }
-  {
-    auto const& src = input[stack];
-    auto dst = data;
-    auto const test = [](sbo<resource> const& dst) {
-      EXPECT_EQ(2u, cont::capacity(dst));
-      EXPECT_EQ(2u, cont::size(dst));
-      EXPECT_EQ(3, dst[0]);
-      EXPECT_EQ(5, dst[1]);
-    };
-    // A2
-    dst[empty] = src;
-    test(dst[empty]);
-    // A5
-    dst[stack] = src;
-    test(dst[stack]);
-    // A8
-    dst[heap] = src;
-    test(dst[heap]);
-  }
-  {
-    auto const& src = input[heap];
-    auto dst = data;
-    auto const test = [](sbo<resource> const& dst) {
-      EXPECT_EQ(3u, cont::capacity(dst));
-      EXPECT_EQ(3u, cont::size(dst));
-      EXPECT_EQ(3, dst[0]);
-      EXPECT_EQ(5, dst[1]);
-      EXPECT_EQ(7, dst[2]);
-    };
-    // A3
-    dst[empty] = src;
-    test(dst[empty]);
-    // A6
-    dst[stack] = src;
-    test(dst[stack]);
-    // A9
-    dst[heap] = src;
-    test(dst[heap]);
+  for (auto const& in : input) {
+    for (auto dst : data) {
+      auto const& s = std::get<src>(in);
+      auto const& test = std::get<func>(in);
+      dst = s;
+      test(dst);
+    }
   }
 }
 
