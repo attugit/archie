f28d5f85bc11fe712747b3dbfad57083ab16a2be
Karol Wozniak <attu.git@email.com>
2015-12-06 13:04:50 +0100

    move fused nth test

--- test/fused/fused_nth_test.cpp
+++ test/fused/fused_nth_test.cpp
@@ -1,9 +1,11 @@
 #include <archie/fused/nth.h>
-#include <archie/test.h>
+#include <memory>
+#include <catch.hpp>
 
+namespace {
 namespace fused = archie::fused;
 
-void canUseNth() {
+TEST_CASE("canUseNth") {
   char a = 0;
   int b = 1;
   float c = 2.0;
@@ -11,24 +13,24 @@ void canUseNth() {
   auto& x = fused::nth<0>(a, b, c);
   auto& y = fused::nth<1>(a, b, c);
   auto& z = fused::nth<2>(a, b, c);
-  EXPECT_EQ(&a, &x);
-  EXPECT_EQ(&b, &y);
-  EXPECT_EQ(&c, &z);
+  REQUIRE(&a == &x);
+  REQUIRE(&b == &y);
+  REQUIRE(&c == &z);
 
   fused::nth<0>(a, b, c) = 7;
-  EXPECT_EQ(7, a);
+  REQUIRE(7 == a);
 }
 
-void canUseConstNth() {
+TEST_CASE("canUseConstNth") {
   const char a = 0;
   const int b = 1;
   const float c = 2.0;
 
   auto const& x = fused::nth<0>(a, b, c);
-  EXPECT_EQ(&a, &x);
+  REQUIRE(&a == &x);
 }
 
-void canWrapNthWithLambda() {
+TEST_CASE("canWrapNthWithLambda") {
   const char a = 0;
   const int b = 1;
   const float c = 2.0;
@@ -36,12 +38,10 @@ void canWrapNthWithLambda() {
   auto f = [](auto&&... args)
                -> decltype(auto) { return fused::nth<0>(std::forward<decltype(args)>(args)...); };
   auto const& x = f(a, b, c);
-  EXPECT_EQ(&a, &x);
+  REQUIRE(&a == &x);
 }
 
-#include <memory>
-
-void canUseNthWithNonCopyable() {
+TEST_CASE("canUseNthWithNonCopyable") {
   auto a = std::make_unique<unsigned>(1u);
   auto b = std::make_unique<int>(2);
   auto c = std::make_unique<char>('3');
@@ -50,26 +50,18 @@ void canUseNthWithNonCopyable() {
   auto const& y = fused::nth<1>(a, b, c);
   auto const& z = fused::nth<2>(a, b, c);
 
-  EXPECT_EQ(*a, *x);
-  EXPECT_EQ(*b, *y);
-  EXPECT_EQ(*c, *z);
+  REQUIRE(*a == *x);
+  REQUIRE(*b == *y);
+  REQUIRE(*c == *z);
 }
 
-void canUseNthWithRValues() {
+TEST_CASE("canUseNthWithRValues") {
   auto x = fused::nth<0>(1u, 2.0, '3');
   auto y = fused::nth<1>(1u, 2.0, '3');
   auto z = fused::nth<2>(1u, 2.0, '3');
 
-  EXPECT_EQ(1u, x);
-  EXPECT_EQ(2.0, y);
-  EXPECT_EQ('3', z);
+  REQUIRE(1u == x);
+  REQUIRE(2.0 == y);
+  REQUIRE('3' == z);
 }
-
-int main() {
-  canUseNth();
-  canUseConstNth();
-  canWrapNthWithLambda();
-  canUseNthWithNonCopyable();
-  canUseNthWithRValues();
-  return 0;
 }
