d63df07b9bb59e321a849f182af3f41fe3db36b7
Karol Wozniak <attu.git@email.com>
2015-12-06 13:35:15 +0100

    move fused::front test

--- test/fused/fused_front_test.cpp
+++ test/fused/fused_front_test.cpp
@@ -1,71 +1,71 @@
 #include <archie/fused/front.h>
 #include <archie/fused/tuple.h>
-#include <archie/test.h>
 #include <memory>
 #include <type_traits>
+#include <catch.hpp>
 
+namespace {
 namespace fused = archie::fused;
 using uptr = std::unique_ptr<unsigned>;
 
-void canUseFrontWithRValue() {
+TEST_CASE("canUseFrontWithRValue") {
   auto a = fused::front(1u, 2.0, '3');
   static_assert(std::is_same<decltype(a), unsigned>::value, "");
-  EXPECT_EQ(1u, a);
+  REQUIRE(1u == a);
 }
 
-void canPassNoncopyableToFront() {
+TEST_CASE("canPassNoncopyableToFront") {
   {
     auto a = fused::front(1u, 2.0, std::make_unique<char>('c'));
     static_assert(std::is_same<decltype(a), unsigned>::value, "");
-    EXPECT_EQ(1u, a);
+    REQUIRE(1u == a);
   }
   {
     auto a = fused::front(std::make_unique<unsigned>(1u), 2.0, '3');
     static_assert(std::is_same<decltype(a), uptr>::value, "");
-    EXPECT_TRUE(a != nullptr);
-    EXPECT_EQ(1u, *a);
+    REQUIRE(a != nullptr);
+    REQUIRE(1u == *a);
   }
   {
     auto a = std::make_unique<unsigned>(1u);
     auto& x = fused::front(a, 2.0, '3');
     static_assert(std::is_same<decltype(x), uptr&>::value, "");
-    EXPECT_TRUE(x != nullptr);
-    EXPECT_EQ(&a, &x);
+    REQUIRE(x != nullptr);
+    REQUIRE(&a == &x);
   }
 }
-
-void canUseBackWithLValue() {
+TEST_CASE("canUseBackWithLValue") {
   {
     auto a = 1u;
     auto x = fused::front(a, 2.0, '3');
     static_assert(std::is_same<decltype(x), unsigned>::value, "");
-    EXPECT_EQ(1u, x);
-    EXPECT_NE(&a, &x);
+    REQUIRE(1u == x);
+    REQUIRE(&a != &x);
   }
   {
     auto const a = 1u;
     auto x = fused::front(a, 2.0, '3');
     static_assert(std::is_same<decltype(x), unsigned>::value, "");
-    EXPECT_EQ(1u, x);
-    EXPECT_NE(&a, &x);
+    REQUIRE(1u == x);
+    REQUIRE(&a != &x);
   }
   {
     auto a = 1u;
     auto& x = fused::front(a, 2.0, '3');
     static_assert(std::is_same<decltype(x), unsigned&>::value, "");
-    EXPECT_EQ(&a, &x);
+    REQUIRE(&a == &x);
   }
   {
     auto const a = 1u;
     auto& x = fused::front(a, 2.0, '3');
     static_assert(std::is_same<decltype(x), unsigned const&>::value, "");
-    EXPECT_EQ(&a, &x);
+    REQUIRE(&a == &x);
   }
   {
     auto a = 1u;
     auto const& x = fused::front(a, 2.0, '3');
     static_assert(std::is_same<decltype(x), unsigned const&>::value, "");
-    EXPECT_EQ(&a, &x);
+    REQUIRE(&a == &x);
   }
 }
 
@@ -79,29 +79,22 @@ auto goo(Ts&&... ts) {
   return foo(fused::make_tuple(std::forward<Ts>(ts)...));
 }
 
-void canWrapFusedFrontWithFunction() {
+TEST_CASE("canWrapFusedFrontWithFunction") {
   {
     auto x = foo(fused::make_tuple(1u, 2.0, '3'));
     static_assert(std::is_same<decltype(x), unsigned>::value, "");
-    EXPECT_EQ(1u, x);
+    REQUIRE(1u == x);
   }
   {
     auto a = fused::make_tuple(1u, 2.0, '3');
     auto const& x = foo(a);
     static_assert(std::is_same<decltype(x), unsigned const&>::value, "");
-    EXPECT_EQ(&fused::get<0>(a), &x);
+    REQUIRE(&fused::get<0>(a) == &x);
   }
   {
     auto x = goo(1u, 2.0, '3');
     static_assert(std::is_same<decltype(x), unsigned>::value, "");
-    EXPECT_EQ(1u, x);
+    REQUIRE(1u == x);
   }
 }
-
-int main() {
-  canUseFrontWithRValue();
-  canPassNoncopyableToFront();
-  canUseBackWithLValue();
-  canWrapFusedFrontWithFunction();
-  return 0;
 }
