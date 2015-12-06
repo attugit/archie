8cc6e891bd3e88ed4f53278b5513d3058c0e7c28
Karol Wozniak <attu.git@email.com>
2015-12-06 13:09:47 +0100

    move meta::index_of test

--- test/meta/meta_index_of_test.cpp
+++ test/meta/meta_index_of_test.cpp
@@ -1,6 +1,8 @@
 #include <archie/meta/index_of.h>
-#include <archie/test.h>
+#include <array>
+#include <catch.hpp>
 
+namespace {
 namespace meta = archie::meta;
 
 static_assert(decltype(meta::type_index<int>(int{}))::value == 0, "");
@@ -8,32 +10,25 @@ static_assert(decltype(meta::type_index<int>(char{}, int{}))::value == 1, "");
 static_assert(decltype(meta::type_index<int>(char{}, int{}, float{}))::value == 1, "");
 static_assert(decltype(meta::type_index<int>(char{}, int{}, float{}, int{}))::value == 1, "");
 
-void canGetIndexOfSimpleType() {
+TEST_CASE("canGetIndexOfSimpleType") {
   char a = '1';
   int b = 2;
   float c = 3;
-  EXPECT_EQ(0, meta::type_index<char>(a, b, c, b));
-  EXPECT_EQ(1, meta::type_index<int>(a, b, c, b));
-  EXPECT_EQ(2, meta::type_index<float>(a, b, c, b));
+  REQUIRE(0 == meta::type_index<char>(a, b, c, b));
+  REQUIRE(1 == meta::type_index<int>(a, b, c, b));
+  REQUIRE(2 == meta::type_index<float>(a, b, c, b));
 }
 
-void canUseIndexOfWithTypeLists() {
-  EXPECT_EQ(0, meta::index_of<char>(meta::type_list<char, int, float>{}));
-  EXPECT_EQ(1, meta::index_of<int>(meta::type_list<char, int, float>{}));
+TEST_CASE("canUseIndexOfWithTypeLists") {
+  REQUIRE(0 == meta::index_of<char>(meta::type_list<char, int, float>{}));
+  REQUIRE(1 == meta::index_of<int>(meta::type_list<char, int, float>{}));
 }
 
-#include <array>
-void canUseIndexOfInConsexpr() {
+TEST_CASE("canUseIndexOfInConsexpr") {
   constexpr auto idx = meta::index_of<char>(meta::type_list<char, int, float>{});
-  EXPECT_EQ(0, idx);
+  REQUIRE(0 == idx);
   constexpr auto cnt = meta::index_of<int>(meta::type_list<char, int, float>{});
   std::array<int, cnt> arr;
-  EXPECT_EQ(sizeof(int), sizeof(arr));
+  REQUIRE(sizeof(int) == sizeof(arr));
 }
-
-int main() {
-  canGetIndexOfSimpleType();
-  canUseIndexOfWithTypeLists();
-  canUseIndexOfInConsexpr();
-  return 0;
 }
