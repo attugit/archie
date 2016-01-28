5fb77b0c18f52010943cf7054dfa64f3cbeec95c
Karol Wozniak <attu.git@email.com>
2016-01-28 23:47:40 +0100

    use static_if in compose

--- include/archie/fused/compose.hpp
+++ include/archie/fused/compose.hpp
@@ -6,6 +6,7 @@
 #include <archie/fused/tail.hpp>
 #include <archie/fused/boolean.hpp>
 #include <archie/fused/conditional.hpp>
+#include <archie/fused/static_if.hpp>
 #include <archie/traits/is_fused_tuple.hpp>
 #include <archie/traits/is_callable.hpp>
 
@@ -26,19 +27,16 @@ namespace fused {
                 typename... Ts,
                 typename = meta::requires<traits::is_fused_tuple<std::remove_reference_t<F>>>>
       constexpr decltype(auto) operator()(F&& f, Ts&&... ts) const {
-        return impl(meta::boolean<(tuple_size(type_tag<F>{}) == 1)>{}, std::forward<F>(f),
-                    std::forward<Ts>(ts)...);
-      }
-
-    private:
-      template <typename F, typename... Ts>
-      constexpr decltype(auto) impl(decltype(True), F&& f, Ts&&... ts) const {
-        return fused::apply(fused::get<0>(std::forward<F>(f)), std::forward<Ts>(ts)...);
-      }
-      template <typename F, typename... Ts>
-      constexpr decltype(auto) impl(decltype(False), F&& f, Ts&&... ts) const {
-        return fused::apply(fused::get<0>(std::forward<F>(f)),
-                            operator()(fused::tail(std::forward<F>(f)), std::forward<Ts>(ts)...));
+        return fused::static_if(meta::boolean<(tuple_size(type_tag<F>{}) == 1)>{})(
+            [](auto&& t, auto&&... args) -> decltype(auto) {
+              return fused::apply(fused::get<0>(std::forward<decltype(t)>(t)),
+                                  std::forward<decltype(args)>(args)...);
+            },
+            [this](auto&& t, auto&&... args) -> decltype(auto) {
+              return fused::apply(fused::get<0>(std::forward<decltype(t)>(t)),
+                                  this->operator()(fused::tail(std::forward<decltype(t)>(t)),
+                                                   std::forward<decltype(args)>(args)...));
+            })(std::forward<F>(f), std::forward<Ts>(ts)...);
       }
     };
   }
