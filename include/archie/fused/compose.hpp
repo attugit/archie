2f7be9b3338b3434ed5c9f5f3296ddcfeeeace95
Karol Wozniak <attu.git@email.com>
2016-01-29 09:11:17 +0100

    use static_if in compose

--- include/archie/fused/compose.hpp
+++ include/archie/fused/compose.hpp
@@ -5,43 +5,36 @@
 #include <archie/fused/apply.hpp>
 #include <archie/fused/tail.hpp>
 #include <archie/fused/boolean.hpp>
-#include <archie/fused/conditional.hpp>
 #include <archie/fused/static_if.hpp>
 #include <archie/traits/is_fused_tuple.hpp>
-#include <archie/traits/is_callable.hpp>
 
 namespace archie {
 namespace fused {
   namespace detail {
-    struct compose_args_ {
-      template <typename F,
-                typename... Ts,
-                typename = meta::requires<traits::is_callable<F, Ts&&...>>>
+    struct compose_ {
+      template <typename F, typename... Ts>
       constexpr decltype(auto) operator()(F&& f, Ts&&... ts) const {
-        return fused::apply(std::forward<F>(f), std::forward<Ts>(ts)...);
-      }
-    };
-
-    struct compose_tuple_ {
-      template <typename F,
-                typename... Ts,
-                typename = meta::requires<traits::is_fused_tuple<std::remove_reference_t<F>>>>
-      constexpr decltype(auto) operator()(F&& f, Ts&&... ts) const {
-        return fused::static_if(meta::boolean<(tuple_size(type_tag<F>{}) == 1)>{})(
-            [](auto&& t, auto&&... args) -> decltype(auto) {
-              return fused::apply(fused::get<0>(std::forward<decltype(t)>(t)),
-                                  std::forward<decltype(args)>(args)...);
+        return fused::static_if(traits::is_fused_tuple<std::decay_t<F>>{})(
+            [this](auto&& tpl, auto&&... xs) -> decltype(auto) {
+              return fused::static_if(meta::boolean<(tuple_size(type_tag<F>{}) == 1)>{})(
+                  [](auto&& t, auto&&... args) -> decltype(auto) {
+                    return fused::apply(fused::get<0>(std::forward<decltype(t)>(t)),
+                                        std::forward<decltype(args)>(args)...);
+                  },
+                  [this](auto&& t, auto&&... args) -> decltype(auto) {
+                    return fused::apply(fused::get<0>(std::forward<decltype(t)>(t)),
+                                        this->operator()(fused::tail(std::forward<decltype(t)>(t)),
+                                                         std::forward<decltype(args)>(args)...));
+                  })(std::forward<decltype(tpl)>(tpl), std::forward<decltype(xs)>(xs)...);
             },
-            [this](auto&& t, auto&&... args) -> decltype(auto) {
-              return fused::apply(fused::get<0>(std::forward<decltype(t)>(t)),
-                                  this->operator()(fused::tail(std::forward<decltype(t)>(t)),
-                                                   std::forward<decltype(args)>(args)...));
+            [](auto&& func, auto&&... args) -> decltype(auto) {
+              return fused::apply(std::forward<decltype(func)>(func),
+                                  std::forward<decltype(args)>(args)...);
             })(std::forward<F>(f), std::forward<Ts>(ts)...);
       }
     };
   }
 
-  constexpr auto const compose =
-      fused::make_conditional(detail::compose_tuple_{}, detail::compose_args_{});
+  static constexpr auto const& compose = meta::instance<detail::compose_>();
 }
 }
