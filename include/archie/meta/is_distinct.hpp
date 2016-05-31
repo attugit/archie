019281505480019f4f584e3c5e82772a21d6a09a
Karol Wozniak <karol.wozniak@email.com>
2016-05-31 21:17:59 +0200

    implement meta::contains and meta::is_distinct with fold expressions

--- include/archie/meta/is_distinct.hpp
+++ include/archie/meta/is_distinct.hpp
@@ -2,31 +2,27 @@
 
 #include <type_traits>
 #include <archie/meta/eval.hpp>
-#include <archie/meta/sequence.hpp>
+#include <archie/meta/logic.hpp>
 #include <archie/fused/boolean.hpp>
 
-namespace archie {
-namespace meta {
-  namespace detail {
+namespace archie::meta
+{
+  template <typename T, typename... U>
+  using contains = any<std::is_same<T, U>...>;
+
+  namespace detail
+  {
     template <typename...>
-    struct is_distinct;
-
-    template <typename Tp, typename... Ts>
-    struct is_distinct<Tp, Ts...>
-        : eval<std::conditional<sequence<Ts...>::template contains<Tp>::value,
-                                decltype(fused::False),
-                                is_distinct<Ts...>>> {
+    struct is_distinct : decltype(fused::True) {
     };
 
-    template <typename Tp>
-    struct is_distinct<Tp> : decltype(fused::True) {
-    };
-
-    template <>
-    struct is_distinct<> : decltype(fused::True) {
+    template <typename T, typename... U>
+    struct is_distinct<T, U...> : eval<std::conditional<contains<T, U...>::value,
+                                                        decltype(fused::False),
+                                                        eval<is_distinct<U...>>>> {
     };
   }
+
   template <typename... Ts>
   using is_distinct = eval<detail::is_distinct<Ts...>>;
-}
 }
