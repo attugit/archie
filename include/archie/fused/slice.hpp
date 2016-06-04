cab4187e7739655c17c799eec4927dee355da803
Karol Wozniak <karol.wozniak@email.com>
2016-06-04 21:30:45 +0200

    add range to slice

--- include/archie/fused/slice.hpp
+++ include/archie/fused/slice.hpp
@@ -9,16 +9,22 @@ namespace archie::fused
 {
   namespace detail
   {
-    template <std::size_t N>
+    template <std::size_t S>
     struct slice_ {
-      template <typename T>
-      constexpr decltype(auto) operator()(T&& t) const
-      {
-        return fused::apply(fused::take<N>, std::forward<T>(t));
-      }
+      template <typename... ids>
+      struct block_ {
+        template <typename T>
+        constexpr decltype(auto) operator()(T&& t) const
+        {
+          return make_tuple(fused::get<S + ids::value>(std::forward<T>(t))...);
+        }
+      };
+      template <std::size_t E>
+      using type = meta::indexable_t<block_, E - S>;
     };
   }
 
-  template <std::size_t N>
-  static constexpr auto const& slice = meta::instance<detail::slice_<N>>();
+  template <std::size_t S, std::size_t E>
+  static constexpr auto const& slice =
+      meta::instance<typename detail::slice_<S>::template type<E>>();
 }
--- test/fused/slice_test.cpp
+++ test/fused/slice_test.cpp
@@ -12,17 +12,19 @@ namespace
   using _0 = utype<0>;
   using _1 = utype<1>;
 
-  using x = decltype(fused::slice<1>(fused::tuple<_0, _1>{}));
+  using x = decltype(fused::slice<0, 1>(fused::tuple<_0, _1>{}));
   static_assert(std::is_same<x, fused::tuple<_0>>::value);
-  using y = decltype(fused::slice<2>(fused::tuple<_0, _1>{}));
+  using y = decltype(fused::slice<0, 2>(fused::tuple<_0, _1>{}));
   static_assert(std::is_same<y, fused::tuple<_0, _1>>::value);
+  using z = decltype(fused::slice<1, 2>(fused::tuple<_0, _1>{}));
+  static_assert(std::is_same<z, fused::tuple<_1>>::value);
 
   TEST_CASE("canTakeElementsFromTuple", "[fused::take]")
   {
     auto a = fused::make_tuple(1, 2.0, '3');
-    auto b = fused::slice<1>(a);
-    auto c = fused::slice<2>(a);
-    auto d = fused::slice<3>(a);
+    auto b = fused::slice<0, 1>(a);
+    auto c = fused::slice<0, 2>(a);
+    auto d = fused::slice<0, 3>(a);
 
     REQUIRE(1 == fused::get<0>(b));
     REQUIRE(1 == fused::get<0>(c));
