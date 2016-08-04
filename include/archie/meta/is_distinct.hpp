058ec9304164929504e09e8afe7b9a97bc8a5b30
Karol Wozniak <karol.wozniak@email.com>
2016-08-04 22:51:12 +0200

    implement is_distinct without recursion inspecting multiple inheritance

--- include/archie/meta/is_distinct.hpp
+++ include/archie/meta/is_distinct.hpp
@@ -4,25 +4,36 @@
 #include <archie/meta/eval.hpp>
 #include <archie/meta/logic.hpp>
 #include <archie/boolean.hpp>
+#include <archie/quote.hpp>
+#include <archie/ignore.hpp>
+#include <archie/type_list.hpp>
+#include <archie/meta/well_formed.hpp>
 
 namespace archie::meta
 {
-  template <typename T, typename... U>
-  using contains = any<std::is_same<T, U>...>;
-
   namespace detail
   {
-    template <typename...>
-    struct is_distinct : decltype(fused::True) {
+    template <std::size_t, typename T>
+    struct indexed_type : quote<T> {
     };
 
-    template <typename T, typename... U>
-    struct is_distinct<T, U...> : eval<std::conditional<contains<T, U...>::value,
-                                                        decltype(fused::False),
-                                                        eval<is_distinct<U...>>>> {
+    template <typename... T>
+    struct inherit : T... {
+    };
+
+    template <typename, typename, typename = well_formed_t<>>
+    struct is_distinct : decltype(fused::False) {
+    };
+
+    template <std::size_t... I, typename... T>
+    struct is_distinct<std::index_sequence<I...>,
+                       type_list<T...>,
+                       well_formed_t<void(decltype(static_cast<quote<T>>(
+                           std::declval<inherit<indexed_type<I, T>...>>()))...)>>
+        : decltype(fused::True) {
     };
   }
 
-  template <typename... Ts>
-  using is_distinct = eval<detail::is_distinct<Ts...>>;
+  template <typename... T>
+  using is_distinct = eval<detail::is_distinct<std::index_sequence_for<T...>, type_list<T...>>>;
 }
--- test/meta/is_distinct_test.cpp
+++ test/meta/is_distinct_test.cpp
@@ -3,9 +3,9 @@
 namespace
 {
   namespace meta = archie::meta;
-  static_assert(meta::is_distinct<int>::value, "");
-  static_assert(meta::is_distinct<int, char, float>::value, "");
-  static_assert(!meta::is_distinct<int, char, int>::value, "");
-  static_assert(!meta::is_distinct<int, int, int>::value, "");
-  static_assert(!meta::is_distinct<char, int, int>::value, "");
+  static_assert(meta::is_distinct<int>::value);
+  static_assert(meta::is_distinct<int, char, float>::value);
+  static_assert(!meta::is_distinct<int, char, int>::value);
+  static_assert(!meta::is_distinct<int, int, int>::value);
+  static_assert(!meta::is_distinct<char, int, int>::value);
 }
